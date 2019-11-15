61a3086f1327e48dd800797be12e419bb3a2d101
Lemuel De Los Santos <lemueld73@gmail.com>
2019-11-15 17:52:06 -0500

    Added PWA Support, Full page caching not done.

--- client/js/main.js
+++ client/js/main.js
@@ -1,11 +1,26 @@
 /* global log, Detect */
 
 define(['jquery', './app', './game'], function($, App, Game) {
-
     var app, body, chatInput, game;
 
-    var load = function() {
+    var install = function() {
+        if (deferredPrompt) {
+            deferredPrompt.prompt();
+            console.log(deferredPrompt);
+            deferredPrompt.userChoice.then(function(choiceResult) {
+                if (choiceResult.outcome === 'accepted') {
+                    console.log('Your PWA has been installed');
+                } else {
+                    console.log('User chose to not install your PWA');
+                }
 
+                deferredPrompt = null;
+                install();
+            });
+        }
+    };
+
+    var load = function() {
         $(document).ready(function() {
             app = new App();
             body = $('body');
@@ -15,36 +30,62 @@ define(['jquery', './app', './game'], function($, App, Game) {
             initGame();
         });
 
+        // This is the "Offline page" service worker
+
+        // Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
+
+        // Check compatibility for the browser we're running this in
+        if ('serviceWorker' in navigator) {
+            if (navigator.serviceWorker.controller) {
+                console.log(
+                    '[PWA Builder] active service worker found, no need to register'
+                );
+            } else {
+                // Register the service worker
+                navigator.serviceWorker
+                    .register('sw.js', {
+                        scope: '../'
+                    })
+                    .then(function(reg) {
+                        console.log(
+                            '[PWA Builder] Service worker has been registered for scope: ' +
+                                reg.scope
+                        );
+                    });
+            }
+        }
+
+        window.addEventListener('beforeinstallprompt', function(e) {
+            // Prevent Chrome 67 and earlier from automatically showing the prompt
+            // e.preventDefault();
+
+            // Stash the event so it can be triggered later.
+            deferredPrompt = e;
+
+            install();
+        });
     };
 
     var addClasses = function() {
         var self = this;
 
-        if (Detect.isWindows())
-            body.addClass('windows');
+        if (Detect.isWindows()) body.addClass('windows');
 
-        if (Detect.isOpera())
-            body.addClass('opera');
+        if (Detect.isOpera()) body.addClass('opera');
 
-        if (Detect.isFirefoxAndroid())
-            chatInput.removeAttr('placeholder');
-
+        if (Detect.isFirefoxAndroid()) chatInput.removeAttr('placeholder');
     };
 
     var initGame = function() {
-
         app.onReady(function() {
             app.sendStatus('Loading game');
 
-            if (app.config.debug)
-                log.info('Loading the main application...');
+            if (app.config.debug) log.info('Loading the main application...');
 
             game = new Game(app);
             app.setGame(game);
         });
-
     };
-
 
     load();
 });
