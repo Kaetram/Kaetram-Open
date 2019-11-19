d48818afa9f74f3826f96e8cb1b1aa6b69405b8e
Lemuel De Los Santos <lemueld73@gmail.com>
2019-11-19 16:24:43 -0500

    Changed ESLint style rules

---
+++ .eslintrc-client.js
@@ -0,0 +1,37 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        jquery: true,
+        node: false,
+        es6: false
+    },
+    extends: ['eslint:recommended', 'standard', 'google'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+    },
+    rules: {
+        curly: ['error', 'multi-or-nest'],
+        indent: ['warn', 4, {
+            SwitchCase: 1
+        }],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+        'operator-linebreak': 0,
+        'no-var': 0,
+        'one-var': 0,
+        'prefer-const': 0,
+        'block-spacing': ['error', 'always'],
+        'comma-dangle': ['error', 'never'],
+        'arrow-parens': 0,
+        'no-undef': 0,
+        'no-unused-vars': 0,
+        'max-len': 0,
+        'no-prototype-builtins': 0,
+        'standard/no-callback-literal': 0,
+        'no-useless-constructor': 0,
+        'require-jsdoc': 0
+    }
+};
