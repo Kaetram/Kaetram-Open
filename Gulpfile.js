bb186f3d0fdc61db76cf6d408dfcd4af0ac541fe
Lemuel De Los Santos <lemueld73@gmail.com>
2019-11-26 07:57:33 -0500

    Added more icons, and more support for mobile browsers

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,47 @@
+/** @format */
+
 const path = require('path');
-const { task, series } = require('gulp');
+const { task, series, parallel, src, dest } = require('gulp');
+const del = require('del');
+const csso = require('gulp-csso');
+const htmlmin = require('gulp-htmlmin');
+const uglify = require('gulp-uglify');
 const { generateSW } = require('workbox-build');
+
+// Gulp task to minify HTML files
+task('pages', async () =>
+    src([path.resolve(__dirname, './client/index.html')])
+        .pipe(
+            htmlmin({
+                caseSensitive: false,
+                collapseInlineTagWhitespace: true,
+                minifyCSS: true,
+                minifyJS: true,
+                collapseWhitespace: true,
+                removeComments: true,
+                sortAttributes: true,
+                sortClassName: false,
+                processScripts: ['application/ld+json']
+            })
+        )
+        .pipe(dest(path.resolve(__dirname, './dist')))
+);
+
+// Gulp task to minify CSS files
+task('styles', async () =>
+    src(path.resolve(__dirname, './client/**/*.css'))
+        .pipe(csso())
+        .pipe(dest(path.resolve(__dirname, './dist/css')))
+);
+
+// Gulp task to minify JavaScript files
+task('scripts', async () =>
+    src(path.resolve(__dirname, './client/**/*.js'))
+        .pipe(uglify())
+        .pipe(dest(path.resolve(__dirname, './dist/js')))
+);
+
+task('clean', async () => del(['dist']));
 
 task('generate-sw', async () => {
     const build = await generateSW({
@@ -21,4 +62,8 @@ task('generate-sw', async () => {
     return build;
 });
 
-exports.default = series('generate-sw');
+exports.default = series(
+    'clean',
+    parallel('pages', 'styles', 'scripts'),
+    'generate-sw'
+);
