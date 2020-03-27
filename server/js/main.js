636486f2e78f5423d7f745c51ddcccc84a6cda58
Flavius Poenaru <flavius256@icloud.com>
2020-03-27 15:36:55 -0600

    Notify Colour Codes
    Former-commit-id: cca5a128ba63e91f7a6d18305bc3e2c1fadfd764

--- server/js/main.js
+++ server/js/main.js
@@ -176,8 +176,6 @@ class Main {
 
 }
 
-
-
 if ( typeof String.prototype.startsWith !== 'function' ) {
     String.prototype.startsWith = function(str) {
         return str.length > 0 && this.substring( 0, str.length ) === str;
