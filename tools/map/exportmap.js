cdcb70b37389c69d17249e45409c78995144799e
Veradictus <Flavius256@icloud.com>
2020-04-07 13:53:21 -0600

    Refactor `exportmap.js`

--- tools/map/exportmap.js
+++ tools/map/exportmap.js
@@ -1,88 +1,77 @@
 #!/usr/bin/env node
 
-config = { debugLevel: 'all' };
+config = { debugLevel: 'all', debug: true };
 
 let Log = require('../../server/js/util/log'),
-    fs = require("fs"),
-    file = require('../file'),
     processMap = require('./processmap'),
-    log = new Log(Log.DEBUG),
-    source = process.argv[2];
+    fs = require("fs");
 
-function getMap() {
-    if (!source)
-        source = 'data/world.json';
+log = new Log();
 
-    file.exists(source, function(exists) {
-        if (!exists) {
-            log.error('The file: ' + source + ' could not be found.');
-            return;
-        }
+class ExportMap {
 
-        fs.readFile(source, function(error, file) {
-            onMap(JSON.parse(file.toString()));
+    constructor() {
+        let self = this;
+
+        self.source = process.argv[2];
+
+        if (!self.source)
+            self.source = 'data/map-refactor.json';
+
+        fs.exists(self.source, (exists) => {
+            if (!exists) {
+                log.error(`The file ${source} could not be found.`);
+                return;
+            }
+
+            fs.readFile(self.source, (error, file) => {
+                self.handleMap(JSON.parse(file.toString()));
+            });
         });
-    });
+    }
+
+    handleMap(data) {
+        let self = this,
+            worldClientJSON = '../../server/data/map/world_client.json',
+            worldServerJSON = '../../server/data/map/world_server.json',
+            clientMapJSON = '../../client/data/maps/map.json',
+            clientMapJS = '../../client/data/maps/map.js';
+
+        let worldClient = self.parse(data, worldClientJSON, 'client');
+
+        self.parse(data, worldServerJSON, 'server');
+        self.parse(data, clientMapJSON, 'info', worldClient);
+        self.parse(data, clientMapJS, 'info', worldClient, true);
+    }
+
+    parse(data, destination, mode, worldClient, isJS) {
+        let self = this,
+            map = processMap(data, { mode: mode });
+
+        if (worldClient)
+            map.depth = worldClient.depth;
+
+        let mapString = JSON.stringify(map);
+
+        if (isJS)
+            mapString = 'let mapData = ' + mapString;
+
+        fs.writeFile(destination, mapString, (error, file) => {
+            if (error)
+                log.error(`An error has occurred while writing map files.`);
+            else
+                log.notice(`[${mode.format()}] Map saved at: ${destination}`);
+        });
+
+        return map;
+    }
+
 }
 
-function onMap(data) {
-    let clientMap = parseClient(data, '../../server/data/map/world_client');
-
-    parseServer(data, '../../server/data/map/world_server');
-    parseInfo(data, '../../client/data/maps/map', clientMap);
+String.prototype.format = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
-function parseClient(data, destination) {
-    let processedMap = processMap(data, { mode: 'client' }),
-        map = JSON.stringify(processedMap);
+module.exports = ExportMap;
 
-    fs.writeFile(destination + '.json', map, function(err, file) {
-        if (err)
-            log.error(JSON.stringify(err));
-        else
-            log.info('[Client] Map saved at: ' + destination + '.json');
-    });
-
-    return processedMap;
-}
-
-function parseServer(data, destination) {
-    let map = JSON.stringify(processMap(data, {
-        mode: 'server'
-    }));
-
-    fs.writeFile(destination + '.json', map, function(err, file) {
-        if (err)
-            log.error(JSON.stringify(err));
-        else
-            log.info('[Server] Map saved at: ' + destination + '.json');
-    });
-}
-
-function parseInfo(data, destination, clientMap) {
-    let mapData = processMap(data, { mode: 'info' });
-
-    if (clientMap)
-        mapData.depth = clientMap.depth;
-
-    let map = JSON.stringify(mapData);
-
-    fs.writeFile(destination + '.json', map, function(err, file) {
-        if (err)
-            log.error(JSON.stringify(err));
-        else
-            log.info('[Client] Map saved at: ' + destination + '.json');
-    });
-
-    map = 'let mapData = ' + map;
-
-    fs.writeFile(destination + '.js', map, function(err, file) {
-        if (err)
-            log.error(JSON.stringify(err));
-        else
-            log.info('[Client] Map saved at: ' + destination + '.js');
-    });
-}
-
-
-getMap();
+new ExportMap();
