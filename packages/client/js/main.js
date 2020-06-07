cce8d191da3cdc46eaf4ec16773d1260939861bc
Brion Fuller <inbreakable@gmail.com>
2020-06-07 14:21:43 -0700

    Started moving client package over to using webpack
    Former-commit-id: ff22ec47af67e570190c5cc54a7f635afb1eafbc

--- packages/client/js/main.js
+++ packages/client/js/main.js
@@ -1,40 +1,41 @@
 /* global log, Detect */
+import $ from 'jquery';
+import App from './app';
+import Game from './game';
 
-define(['jquery', './app', './game'], function($, App, Game) {
-    var app, body, chatInput, game;
+var app, body, chatInput, game;
 
-    var load = function() {
-        $(document).ready(function() {
-            app = new App();
-            body = $('body');
-            chatInput = $('#chatInput');
+var load = function () {
+    $(document).ready(function () {
+        app = new App();
+        body = $('body');
+        chatInput = $('#chatInput');
 
-            addClasses();
-            initGame();
-        });
-    };
+        addClasses();
+        initGame();
+    });
+};
 
-    var addClasses = function() {
-        var self = this;
+var addClasses = function () {
+    var self = this;
 
-        if (Detect.isWindows()) body.addClass('windows');
+    if (Detect.isWindows()) body.addClass('windows');
 
-        if (Detect.isOpera()) body.addClass('opera');
+    if (Detect.isOpera()) body.addClass('opera');
 
-        if (Detect.isFirefoxAndroid()) chatInput.removeAttr('placeholder');
-    };
+    if (Detect.isFirefoxAndroid()) chatInput.removeAttr('placeholder');
+};
 
-    var initGame = function() {
-        app.onReady(function() {
-            app.sendStatus('Loading game');
+var initGame = function () {
+    app.onReady(function () {
+        app.sendStatus('Loading game');
 
-            if (app.config.debug) log.info('Loading the main application...');
-            if (app.config.worldSwitch) $('#worlds-switch').show();
+        if (app.config.debug) log.info('Loading the main application...');
+        if (app.config.worldSwitch) $('#worlds-switch').show();
 
-            game = new Game(app);
-            app.setGame(game);
-        });
-    };
+        game = new Game(app);
+        app.setGame(game);
+    });
+};
 
-    load();
-});
+load();
