58cf43e919bad1fe131c0c74f2aef75892e74d1f
Brion Fuller <blfullerii@gmail.com>
2020-06-09 21:57:21 -0700

    Updated all files to use imports the client runs but fails in multiple spots

--- packages/client/js/renderer/camera.js
+++ packages/client/js/renderer/camera.js
@@ -1,280 +1,285 @@
-/* global Modules, log */
+import Modules from '../utils/modules';
 
-define(function() {
+export default class Camera {
+    constructor(renderer) {
+        var self = this;
 
-    return Class.extend({
+        self.renderer = renderer;
+        self.map = renderer.map;
+        self.app = renderer.game.app;
 
-        init: function(renderer) {
-            var self = this;
+        self.offset = 0.5;
+        self.x = 0;
+        self.y = 0;
 
-            self.renderer = renderer;
-            self.map = renderer.map;
-            self.app = renderer.game.app;
+        self.dX = 0;
+        self.dY = 0;
 
-            self.offset = 0.5;
-            self.x = 0;
-            self.y = 0;
+        self.gridX = 0;
+        self.gridY = 0;
 
-            self.dX = 0;
-            self.dY = 0;
+        self.prevGridX = 0;
+        self.prevGridY = 0;
 
-            self.gridX = 0;
-            self.gridY = 0;
+        self.tileSize = self.renderer.tileSize;
 
-            self.prevGridX = 0;
-            self.prevGridY = 0;
+        self.speed = 1;
+        self.panning = false;
+        self.centered = true;
+        self.player = null;
 
-            self.tileSize = self.renderer.tileSize;
+        self.lockX = false;
+        self.lockY = false;
 
-            self.speed = 1;
-            self.panning = false;
-            self.centered = true;
-            self.player = null;
+        self.update();
+    }
 
-            self.lockX = false;
-            self.lockY = false;
+    update() {
+        var self = this,
+            scale = self.renderer.getScale(),
+            borderWidth = self.app.border.width(),
+            borderHeight = self.app.border.height(),
+            factorWidth = Math.ceil(borderWidth / self.tileSize / scale),
+            factorHeight = Math.ceil(borderHeight / self.tileSize / scale);
 
-            self.update();
-        },
+        self.gridWidth = factorWidth;
+        self.gridHeight = factorHeight;
 
-        update: function() {
-            var self = this,
-                scale = self.renderer.getScale(),
-                borderWidth = self.app.border.width(),
-                borderHeight = self.app.border.height(),
-                factorWidth = Math.ceil(borderWidth / self.tileSize / scale),
-                factorHeight = Math.ceil(borderHeight / self.tileSize / scale);
+        self.borderX =
+            self.map.width * self.tileSize - self.gridWidth * self.tileSize;
+        self.borderY =
+            self.map.height * self.tileSize - self.gridHeight * self.tileSize;
+    }
 
-            self.gridWidth = factorWidth;
-            self.gridHeight = factorHeight;
+    setPosition(x, y) {
+        var self = this;
 
-            self.borderX = (self.map.width * self.tileSize) - self.gridWidth * self.tileSize;
-            self.borderY = (self.map.height * self.tileSize) - self.gridHeight * self.tileSize;
-        },
+        self.x = x;
+        self.y = y;
 
-        setPosition: function(x, y) {
-            var self = this;
+        self.prevGridX = self.gridX;
+        self.prevGridY = self.gridY;
 
-            self.x = x;
-            self.y = y;
+        self.gridX = Math.floor(x / 16);
+        self.gridY = Math.floor(y / 16);
+    }
 
-            self.prevGridX = self.gridX;
-            self.prevGridY = self.gridY;
+    clip() {
+        this.setGridPosition(Math.round(this.x / 16), Math.round(this.y / 16));
+    }
 
-            self.gridX = Math.floor(x / 16);
-            self.gridY = Math.floor(y / 16);
-        },
+    center() {
+        var self = this;
 
-        clip: function() {
-            this.setGridPosition(Math.round(this.x / 16), Math.round(this.y / 16));
-        },
+        if (self.centered) return;
 
-        center: function() {
-            var self = this;
+        self.centered = true;
+        self.centreOn(self.player);
 
-            if (self.centered)
-                return;
+        self.renderer.verifyCentration();
+    }
 
-            self.centered = true;
-            self.centreOn(self.player);
+    decenter() {
+        var self = this;
 
-            self.renderer.verifyCentration();
-        },
+        if (!self.centered) return;
 
-        decenter: function() {
-            var self = this;
+        self.clip();
+        self.centered = false;
 
-            if (!self.centered)
-                return;
+        self.renderer.verifyCentration();
+    }
 
-            self.clip();
-            self.centered = false;
+    setGridPosition(x, y) {
+        var self = this;
 
-            self.renderer.verifyCentration();
-        },
+        self.prevGridX = self.gridX;
+        self.prevGridY = self.gridY;
 
-        setGridPosition: function(x, y) {
-            var self = this;
+        self.gridX = x;
+        self.gridY = y;
 
-            self.prevGridX = self.gridX;
-            self.prevGridY = self.gridY;
+        self.x = self.gridX * 16;
+        self.y = self.gridY * 16;
+    }
 
-            self.gridX = x;
-            self.gridY = y;
+    setPlayer(player) {
+        var self = this;
 
-            self.x = self.gridX * 16;
-            self.y = self.gridY * 16;
-        },
+        self.player = player;
 
-        setPlayer: function(player) {
-            var self = this;
+        self.centreOn(self.player);
+    }
 
-            self.player = player;
+    handlePanning(direction) {
+        var self = this;
 
-            self.centreOn(self.player);
-        },
+        if (!self.panning) return;
 
-        handlePanning: function(direction) {
-            var self = this;
+        switch (direction) {
+            case Modules.Keys.Up:
+                self.setPosition(self.x, self.y - 1);
+                break;
 
-            if (!self.panning)
-                return;
+            case Modules.Keys.Down:
+                self.setPosition(self.x, self.y + 1);
+                break;
 
-            switch (direction) {
-                case Modules.Keys.Up:
-                    self.setPosition(self.x, self.y - 1);
-                    break;
+            case Modules.Keys.Left:
+                self.setPosition(self.x - 1, self.y);
+                break;
 
-                case Modules.Keys.Down:
-                    self.setPosition(self.x, self.y + 1);
-                    break;
+            case Modules.Keys.Right:
+                self.setPosition(self.x + 1, self.y);
+                break;
+        }
+    }
 
-                case Modules.Keys.Left:
-                    self.setPosition(self.x - 1, self.y);
-                    break;
+    centreOn(entity) {
+        var self = this;
 
-                case Modules.Keys.Right:
-                    self.setPosition(self.x + 1, self.y);
-                    break;
-            }
-        },
+        if (!entity) return;
 
-        centreOn: function(entity) {
-            var self = this;
+        var width = Math.floor(self.gridWidth / 2),
+            height = Math.floor(self.gridHeight / 2),
+            nextX = entity.x - width * self.tileSize,
+            nextY = entity.y - height * self.tileSize;
 
-            if (!entity)
-                return;
-
-            var width = Math.floor(self.gridWidth / 2),
-                height = Math.floor(self.gridHeight / 2),
-                nextX = entity.x - (width * self.tileSize),
-                nextY = entity.y - (height * self.tileSize);
-
-            if (nextX >= 0 && nextX <= self.borderX && !self.lockX) {
-                self.x = nextX;
-                self.gridX = Math.round(entity.x / 16) - width;
-            } else
-                self.offsetX(nextX);
-
-            if (nextY >= 0 && nextY <= self.borderY && !self.lockY) {
-                self.y = nextY;
-                self.gridY = Math.round(entity.y / 16) - height;
-            } else
-                self.offsetY(nextY);
-
-        },
-
-        forceCentre: function(entity) {
-            var self = this;
-
-            if (!entity)
-                return;
-
-            var width = Math.floor(self.gridWidth / 2),
-                height = Math.floor(self.gridHeight / 2);
-
-            self.x = entity.x - (width * self.tileSize);
+        if (nextX >= 0 && nextX <= self.borderX && !self.lockX) {
+            self.x = nextX;
             self.gridX = Math.round(entity.x / 16) - width;
+        } else self.offsetX(nextX);
 
-            self.y = entity.y - (height * self.tileSize);
+        if (nextY >= 0 && nextY <= self.borderY && !self.lockY) {
+            self.y = nextY;
             self.gridY = Math.round(entity.y / 16) - height;
-        },
+        } else self.offsetY(nextY);
+    }
 
-        offsetX: function(nextX) {
-            var self = this;
+    forceCentre(entity) {
+        var self = this;
 
-            if (nextX <= 16) {
-                self.x = 0;
-                self.gridX = 0;
-            } else if (nextX >= self.borderX) {
-                self.x = self.borderX;
-                self.gridX = Math.round(self.borderX / 16);
-            }
-        },
+        if (!entity) return;
 
-        offsetY: function(nextY) {
-            var self = this;
+        var width = Math.floor(self.gridWidth / 2),
+            height = Math.floor(self.gridHeight / 2);
 
-            if (nextY <= 16) {
-                self.y = 0;
-                self.gridY = 0;
-            } else if (nextY >= self.borderY) {
-                self.y = self.borderY;
-                self.gridY = Math.round(self.borderY / 16);
-            }
-        },
+        self.x = entity.x - width * self.tileSize;
+        self.gridX = Math.round(entity.x / 16) - width;
 
-        zone: function(direction) {
-            var self = this;
+        self.y = entity.y - height * self.tileSize;
+        self.gridY = Math.round(entity.y / 16) - height;
+    }
 
-            switch (direction) {
-                case Modules.Orientation.Up:
+    offsetX(nextX) {
+        var self = this;
 
-                    self.setGridPosition(self.gridX, self.gridY - self.gridHeight + 3);
+        if (nextX <= 16) {
+            self.x = 0;
+            self.gridX = 0;
+        } else if (nextX >= self.borderX) {
+            self.x = self.borderX;
+            self.gridX = Math.round(self.borderX / 16);
+        }
+    }
 
-                    break;
+    offsetY(nextY) {
+        var self = this;
 
-                case Modules.Orientation.Down:
+        if (nextY <= 16) {
+            self.y = 0;
+            self.gridY = 0;
+        } else if (nextY >= self.borderY) {
+            self.y = self.borderY;
+            self.gridY = Math.round(self.borderY / 16);
+        }
+    }
 
-                    self.setGridPosition(self.gridX, self.gridY + self.gridHeight - 3);
+    zone(direction) {
+        var self = this;
 
-                    break;
+        switch (direction) {
+            case Modules.Orientation.Up:
+                self.setGridPosition(
+                    self.gridX,
+                    self.gridY - self.gridHeight + 3
+                );
 
-                case Modules.Orientation.Right:
+                break;
 
-                    self.setGridPosition(self.gridX + self.gridWidth - 3, self.gridY);
+            case Modules.Orientation.Down:
+                self.setGridPosition(
+                    self.gridX,
+                    self.gridY + self.gridHeight - 3
+                );
 
-                    break;
+                break;
 
-                case Modules.Orientation.Left:
+            case Modules.Orientation.Right:
+                self.setGridPosition(
+                    self.gridX + self.gridWidth - 3,
+                    self.gridY
+                );
 
-                    self.setGridPosition(self.gridX - self.gridWidth + 3, self.gridY);
+                break;
 
-                    break;
-            }
+            case Modules.Orientation.Left:
+                self.setGridPosition(
+                    self.gridX - self.gridWidth + 3,
+                    self.gridY
+                );
 
-            self.zoneClip();
-        },
-
-        zoneClip: function() {
-            var self = this;
-
-            /**
-             * Clip the map to the boundaries of the map if
-             * we zone somewhere out of the limitations.
-             */
-
-            if (self.gridX < 0)
-                self.setGridPosition(0, self.gridY);
-
-            if (self.gridX > self.map.width)
-                self.setGridPosition(self.map.width, self.gridY);
-
-            if (self.gridY < 0)
-                self.setGridPosition(self.gridX, 0);
-
-            if (self.gridY > self.map.height)
-                self.setGridPosition(self.gridX, self.map.height);
-        },
-
-        forEachVisiblePosition: function(callback, offset) {
-            var self = this;
-
-            if (!offset)
-                offset = 1;
-
-            for(var y = self.gridY - offset, maxY = y + self.gridHeight + (offset * 2); y < maxY; y++)
-                for(var x = self.gridX - offset, maxX = x + self.gridWidth + (offset * 2); x < maxX; x++)
-                    callback(x, y);
-        },
-
-        isVisible: function(x, y, offset, offset2) {
-            return x > this.gridX - offset &&
-                    x < this.gridX + this.gridWidth &&
-                    y > this.gridY - offset &&
-                    y < this.gridY + this.gridHeight + offset2;
+                break;
         }
 
-    });
+        self.zoneClip();
+    }
 
-});
+    zoneClip() {
+        var self = this;
+
+        /**
+         * Clip the map to the boundaries of the map if
+         * we zone somewhere out of the limitations.
+         */
+
+        if (self.gridX < 0) self.setGridPosition(0, self.gridY);
+
+        if (self.gridX > self.map.width)
+            self.setGridPosition(self.map.width, self.gridY);
+
+        if (self.gridY < 0) self.setGridPosition(self.gridX, 0);
+
+        if (self.gridY > self.map.height)
+            self.setGridPosition(self.gridX, self.map.height);
+    }
+
+    forEachVisiblePosition(callback, offset) {
+        var self = this;
+
+        if (!offset) offset = 1;
+
+        for (
+            var y = self.gridY - offset,
+                maxY = y + self.gridHeight + offset * 2;
+            y < maxY;
+            y++
+        )
+            for (
+                var x = self.gridX - offset,
+                    maxX = x + self.gridWidth + offset * 2;
+                x < maxX;
+                x++
+            )
+                callback(x, y);
+    }
+
+    isVisible(x, y, offset, offset2) {
+        return (
+            x > this.gridX - offset &&
+            x < this.gridX + this.gridWidth &&
+            y > this.gridY - offset &&
+            y < this.gridY + this.gridHeight + offset2
+        );
+    }
+}
--- packages/client/js/renderer/grids.js
+++ packages/client/js/renderer/grids.js
@@ -1,107 +1,102 @@
-/* global log */
+import log from '../lib/log';
 
-define(function() {
+export default class Grids {
+    constructor(map) {
+        var self = this;
 
-    return Class.extend({
+        self.map = map;
 
-        init: function(map) {
-            var self = this;
+        self.renderingGrid = [];
+        self.pathingGrid = [];
+        self.itemGrid = [];
 
-            self.map = map;
+        self.load();
+    }
 
-            self.renderingGrid = [];
-            self.pathingGrid = [];
-            self.itemGrid = [];
+    load() {
+        var self = this;
 
-            self.load();
-        },
+        for (var i = 0; i < self.map.height; i++) {
+            self.renderingGrid[i] = [];
+            self.pathingGrid[i] = [];
+            self.itemGrid[i] = [];
 
-        load: function() {
-            var self = this;
-
-            for (var i = 0; i < self.map.height; i++) {
-                self.renderingGrid[i] = [];
-                self.pathingGrid[i] = [];
-                self.itemGrid[i] = [];
-
-                for (var j = 0; j < self.map.width; j++) {
-                    self.renderingGrid[i][j] = {};
-                    self.pathingGrid[i][j] = self.map.grid[i][j];
-                    self.itemGrid[i][j] = {};
-                }
-            }
-
-            if (self.map.game.isDebug())
-                log.info('Finished generating grids.');
-        },
-
-        resetPathingGrid: function() {
-            var self = this;
-
-            self.pathingGrid = [];
-
-            for (var i = 0; i < self.map.height; i++) {
-                self.pathingGrid[i] = [];
-
-                for (var j = 0; j < self.map.width; j++)
-                    self.pathingGrid[i][j] = self.map.grid[i][j];
-
-            }
-        },
-
-        addToRenderingGrid: function(entity, x, y) {
-            var self = this;
-
-            if (!self.map.isOutOfBounds(x, y))
-                self.renderingGrid[y][x][entity.id] = entity;
-        },
-
-        addToPathingGrid: function(x, y) {
-            this.pathingGrid[y][x] = 1;
-        },
-
-        addToItemGrid: function(item, x, y) {
-            var self = this;
-
-            if (item && self.itemGrid[y][x])
-                self.itemGrid[y][x][item.id] = item;
-        },
-
-        removeFromRenderingGrid: function(entity, x, y) {
-            var self = this;
-
-            if (entity && self.renderingGrid[y][x] && entity.id in self.renderingGrid[y][x])
-                delete self.renderingGrid[y][x][entity.id];
-        },
-
-        removeFromPathingGrid: function(x, y) {
-            this.pathingGrid[y][x] = 0;
-        },
-
-        removeFromMapGrid: function(x, y) {
-            this.map.grid[y][x] = 0;
-        },
-
-        removeFromItemGrid: function(item, x, y) {
-            var self = this;
-
-            if (item && self.itemGrid[y][x][item.id])
-                delete self.itemGrid[y][x][item.id];
-        },
-
-        removeEntity: function(entity) {
-            var self = this;
-
-            if (entity) {
-                self.removeFromPathingGrid(entity.gridX, entity.gridY);
-                self.removeFromRenderingGrid(entity, entity.gridX, entity.gridY);
-
-                if (entity.nextGridX > -1 && entity.nextGridY > -1) {
-                    self.removeFromPathingGrid(entity.nextGridX, entity.nextGridY);
-                }
+            for (var j = 0; j < self.map.width; j++) {
+                self.renderingGrid[i][j] = {};
+                self.pathingGrid[i][j] = self.map.grid[i][j];
+                self.itemGrid[i][j] = {};
             }
         }
 
-    });
+        if (self.map.game.isDebug()) log.info('Finished generating grids.');
+    }
 
-});
+    resetPathingGrid() {
+        var self = this;
+
+        self.pathingGrid = [];
+
+        for (var i = 0; i < self.map.height; i++) {
+            self.pathingGrid[i] = [];
+
+            for (var j = 0; j < self.map.width; j++)
+                self.pathingGrid[i][j] = self.map.grid[i][j];
+        }
+    }
+
+    addToRenderingGrid(entity, x, y) {
+        var self = this;
+
+        if (!self.map.isOutOfBounds(x, y))
+            self.renderingGrid[y][x][entity.id] = entity;
+    }
+
+    addToPathingGrid(x, y) {
+        this.pathingGrid[y][x] = 1;
+    }
+
+    addToItemGrid(item, x, y) {
+        var self = this;
+
+        if (item && self.itemGrid[y][x]) self.itemGrid[y][x][item.id] = item;
+    }
+
+    removeFromRenderingGrid(entity, x, y) {
+        var self = this;
+
+        if (
+            entity &&
+            self.renderingGrid[y][x] &&
+            entity.id in self.renderingGrid[y][x]
+        )
+            delete self.renderingGrid[y][x][entity.id];
+    }
+
+    removeFromPathingGrid(x, y) {
+        this.pathingGrid[y][x] = 0;
+    }
+
+    removeFromMapGrid(x, y) {
+        this.map.grid[y][x] = 0;
+    }
+
+    removeFromItemGrid(item, x, y) {
+        var self = this;
+
+        if (item && self.itemGrid[y][x][item.id])
+            delete self.itemGrid[y][x][item.id];
+    }
+
+    removeEntity(entity) {
+        var self = this;
+
+        if (entity) {
+            self.removeFromPathingGrid(entity.gridX, entity.gridY);
+            self.removeFromRenderingGrid(entity, entity.gridX, entity.gridY);
+
+            if (entity.nextGridX > -1 && entity.nextGridY > -1) {
+                self.removeFromPathingGrid(entity.nextGridX, entity.nextGridY);
+            }
+        }
+    }
+}
