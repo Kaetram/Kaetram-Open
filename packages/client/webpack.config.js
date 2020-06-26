d6c1e57f03945313d863209f284b60c765887cc7
Adam Sawicki <adamsawicki89@gmail.com>
2020-06-26 14:47:27 +0200

    Copy assets to client build
    Former-commit-id: 37a3c6e6a8cd03a2978bea1d91b63b56cc6b5545

--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -17,5 +18,18 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({ template: path.join(__dirname, 'index.html') }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'css', to: 'css' },
+                { from: 'img', to: 'img' },
+                { from: 'fonts', to: 'fonts' },
+                { from: 'data', to: 'data' },
+                { from: 'sw.js', to: 'sw.js' },
+                { from: 'favicon.ico', to: 'favicon.ico' },
+                { from: 'manifest.json', to: 'manifest.json' },
+                { from: 'js/map/mapworker.js', to: 'js/map/mapworker.js' },
+                { from: 'js/lib/underscore.min.js', to: 'js/lib/underscore.min.js' },
+            ],
+        }),
     ],
 };
