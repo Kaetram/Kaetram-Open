31de526686a90e252f0a2dd660f6f6d3ec67d050
Lemuel De Los Santos <lemueld73@gmail.com>
2020-09-12 09:47:01 -0400

    ⚡️ Service worker generated by webpack and Workbox Fixes #29

--- packages/client/webpack.config.ts
+++ packages/client/webpack.config.ts
@@ -1,14 +1,20 @@
 import path from 'path';
 import dotEnvExtended from 'dotenv-extended';
+import dotenvParseVariables from 'dotenv-parse-variables';
 import { WebpackOptions } from 'webpack/declarations/WebpackOptions';
 
 import { HotModuleReplacementPlugin, DefinePlugin } from 'webpack';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
-import dotenvParseVariables from 'dotenv-parse-variables';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-// import CopyPlugin from 'copy-webpack-plugin';
-
+import { InjectManifest } from 'workbox-webpack-plugin';
+import FaviconsWebpackPlugin from 'favicons-webpack-plugin';
+import SitemapPlugin from 'sitemap-webpack-plugin';
+import RobotstxtPlugin from 'robotstxt-webpack-plugin';
+import ResourceHintWebpackPlugin from 'resource-hints-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+
+import { version, description } from '../../package.json';
 
 const env = dotenvParseVariables(dotEnvExtended.load());
 const { NODE_ENV } = env;
@@ -16,6 +22,8 @@ const { NODE_ENV } = env;
 const inProduction = NODE_ENV === 'production';
 
 const exclude = /(node_modules|bower_components)/;
+
+const iconPath = path.resolve(__dirname, 'img/icon.png');
 
 const config: WebpackOptions = {
     name: 'Client',
@@ -79,6 +87,9 @@ const config: WebpackOptions = {
         new DefinePlugin({
             'process.env': JSON.stringify(env)
         }),
+        new CopyWebpackPlugin({
+            patterns: ['img/icon.png']
+        }),
         new HtmlWebpackPlugin({
             template: path.join(__dirname, './index.html'),
             minify: inProduction
@@ -92,9 +103,33 @@ const config: WebpackOptions = {
                   }
                 : false
         }),
+        new FaviconsWebpackPlugin({
+            logo: iconPath,
+            prefix: '',
+            favicons: {
+                appName: 'Kaetram',
+                appDescription: description,
+                version,
+                background: '#000000',
+                theme_color: '#000000',
+                display: 'fullscreen',
+                start_url: '/?utm_source=a2hs',
+                pixel_art: true
+            }
+        }),
+        new SitemapPlugin('https://kaetram.com', ['/']),
+        new RobotstxtPlugin({
+            sitemap: 'https://kaetram.com/sitemap.xml',
+            host: 'https://kaetram.com'
+        }),
         new MiniCssExtractPlugin({
             filename: '[name].[hash].css',
             chunkFilename: '[name].[chunkhash].css'
+        }),
+        new ResourceHintWebpackPlugin(),
+        new InjectManifest({
+            swSrc: './lib/sw.ts',
+            maximumFileSizeToCacheInBytes: 5e6
         })
     ]
 };
