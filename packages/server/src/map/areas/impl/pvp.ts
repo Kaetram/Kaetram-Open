af41cac2031a35c19a8d464624b12464af5f6c79
Flavius Poenaru <Flavius256@icloud.com>
2021-02-19 15:42:05 -0700

    Dynamic Loading of Object Areas

---
+++ packages/server/src/map/areas/impl/pvp.ts
@@ -0,0 +1,16 @@
+import Area from '../area';
+import Areas from '../areas';
+
+import World from '../../../game/world';
+
+export default class PVP extends Areas {
+
+    constructor(data: any, world?: World) {
+        super(data, world);
+
+        super.load(this.data);
+
+        super.message('PVP');
+    }
+
+}
