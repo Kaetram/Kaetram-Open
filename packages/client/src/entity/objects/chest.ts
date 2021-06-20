8731c2f8ab13fe237a230b93eaecb53074878cae
Lemuel DLS <lemueld73@gmail.com>
2021-06-20 01:33:09 -0400

    🎨 Enable `noImplicitOverride`

--- packages/client/src/entity/objects/chest.ts
+++ packages/client/src/entity/objects/chest.ts
@@ -1,13 +1,13 @@
 import Entity from '../entity';
 
 export default class Chest extends Entity {
-    public readonly type = 'chest';
+    public override readonly type = 'chest';
 
     public constructor(id: string, kind: string) {
         super(id, kind);
     }
 
-    public idle(): void {
+    public override idle(): void {
         this.setAnimation('idle_down', 150);
     }
 }
