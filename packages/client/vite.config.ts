d6b26a0a92cc30a150a8b2c9f4fade86521b03dc
Lemuel DLS <lemueld73@gmail.com>
2021-08-03 12:11:04 -0400

    🔥 Remove `HUB_HOST` for `HOST`

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -28,7 +28,6 @@ export default defineConfig(({ command }) => {
             'ssl',
             'worldSwitch',
             'hubEnabled',
-            'hubHost',
             'hubPort'
         ],
         env = {} as Config;
