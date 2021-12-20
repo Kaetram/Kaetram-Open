33351482d1cf333b77a81c9ed3cc45ca2302738d
Flavius Poenaru <Flavius256@icloud.com>
2021-12-20 13:12:38 -0700

    Dynamic tiles region integration

--- packages/server/src/game/map/areas/impl/dynamic.ts
+++ packages/server/src/game/map/areas/impl/dynamic.ts
@@ -1,18 +1,38 @@
+import _ from 'lodash';
+
 import World from '../../../world';
 import Area from '../area';
 import Areas from '../areas';
 
 import type { ProcessedArea } from '@kaetram/common/types/map';
+import log from '@kaetram/common/util/log';
+
+type Position = { x: number; y: number };
 
 export default class Dynamic extends Areas {
     public constructor(data: ProcessedArea[], world: World) {
         super(data, world);
 
         super.load(this.data, (area: Area, rawData) => {
-            console.log(area);
-            console.log(rawData);
+            area.mapping = rawData.mapping!;
+            area.quest = rawData.quest!;
+            area.achievement = rawData.achievement!;
         });
 
-        super.message('dynamic');
+        this.link();
+
+        log.info(`Loaded ${this.areas.length / 2} dynamic areas.`);
+    }
+
+    /**
+     * Links all the dynamic regions to their mapping counterparts.
+     */
+
+    private link(): void {
+        _.each(this.areas, (area: Area) => {
+            if (!area.mapping) return;
+
+            area.mappedArea = this.get(area.mapping);
+        });
     }
 }
