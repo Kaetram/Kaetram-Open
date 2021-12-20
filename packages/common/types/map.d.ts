2898caece2304cc0565d256fbc6aefd2e25104e8
Flavius Poenaru <Flavius256@icloud.com>
2021-12-20 13:12:23 -0700

    Map Update

--- packages/common/types/map.d.ts
+++ packages/common/types/map.d.ts
@@ -38,6 +38,10 @@ export interface ProcessedArea {
     // Overlay
     darkness?: number;
     fog?: string;
+
+    // Dynamic
+    quest?: number;
+    mapping?: number;
 }
 
 export interface Entities {
