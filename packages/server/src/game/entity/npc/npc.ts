c858d7f42d567025e55bbf64297aabf45cbbe718
Flavius Poenaru <Flavius256@icloud.com>
2021-12-23 01:25:50 -0700

    Refactor NPC and remove it from loader

--- packages/server/src/game/entity/npc/npc.ts
+++ packages/server/src/game/entity/npc/npc.ts
@@ -3,13 +3,36 @@ import Entity from '../entity';
 import type Player from '../character/player/player';
 import Utils from '@kaetram/common/util/utils';
 import { Modules } from '@kaetram/common/network';
-import NPCs from '@kaetram/server/src/info/npcs';
+
+import { NPCData } from '@kaetram/common/types/npc';
+
+import rawData from '../../../../data/npcs.json';
+import log from '@kaetram/common/util/log';
+
+type RawData = {
+    [key: string]: NPCData;
+};
 
 export default class NPC extends Entity {
     // talkIndex = 0;
 
+    private data: NPCData;
+
+    private text: string[] = [];
+    private role?: string;
+
     public constructor(key: string, x: number, y: number) {
         super(Utils.createInstance(Modules.EntityType.NPC), key, x, y);
+
+        this.data = (rawData as RawData)[key];
+
+        if (!this.data) {
+            log.error(`Could not find data for ${key}.`);
+            return;
+        }
+
+        this.text = this.data.text || this.text;
+        this.role = this.data.role!;
     }
 
     public talk(messages: string[], player?: Player): string | undefined {
