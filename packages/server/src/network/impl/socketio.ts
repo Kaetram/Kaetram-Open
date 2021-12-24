2452f31f62441763caf41088c3bceacd63d65dc8
Flavius Poenaru <Flavius256@icloud.com>
2021-12-24 02:18:07 -0700

    Refactor and document the socket system
    Former-commit-id: f6ed704ae2a13d4eba31bee46f6f95aa1998842e

--- packages/server/src/network/impl/socketio.ts
+++ packages/server/src/network/impl/socketio.ts
@@ -21,24 +21,37 @@ export default class SocketIO extends WebSocket {
             }
         });
 
-        this.server.on('connection', (socket) => {
-            if (socket.handshake.headers['cf-connecting-ip'])
-                socket.conn.remoteAddress = socket.handshake.headers['cf-connecting-ip'];
+        this.server.on('connection', this.handleConnection.bind(this));
+    }
 
-            log.info(`Received connection from: ${socket.conn.remoteAddress}.`);
+    /**
+     * Unsure what type to specify for the socket here. If anyone has the time
+     * and energy to really figure it out go on ahead please.
+     * I can't be bothered to focus too much on SocketIO since the intention
+     * is to completely deprecate it once Godot client becomes more advanced.
+     * TODO - Figure out the types for this.
+     */
 
-            let connection = new Connection(
-                Utils.createInstance(Modules.EntityType.Player),
-                this.type,
-                socket as AnySocket,
-                this.socketHandler
-            );
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    private handleConnection(socket: any): void {
+        let remoteAddress =
+            (socket.handshake.headers['cf-connecting-ip'] as string) || '69.69.69.69';
 
-            socket.on('client', (data) => {
-                if (!this.verifyVersion(connection, data.gVer)) return;
+        log.info(`Received connection from: ${remoteAddress}.`);
 
-                this.addCallback?.(connection);
-            });
+        let connection = new Connection(
+            Utils.createInstance(Modules.EntityType.Player),
+            this.type,
+            remoteAddress,
+            socket as AnySocket,
+            this.socketHandler
+        );
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        socket.on('client', (data: any) => {
+            if (!this.verifyVersion(connection, data.gVer)) return;
+
+            this.addCallback?.(connection);
         });
     }
 }
--- packages/server/src/network/impl/ws.ts
+++ packages/server/src/network/impl/ws.ts
@@ -1,4 +1,6 @@
-import ws from 'ws';
+import ws, { Server as WSServer } from 'ws';
+
+import http from 'http';
 
 import config from '@kaetram/common/config';
 import log from '@kaetram/common/util/log';
@@ -19,28 +21,45 @@ export default class WS extends WebSocket {
     public constructor(socketHandler: SocketHandler) {
         super(config.host, config.websocketPort, 'WebSocket', socketHandler);
 
-        let server = new ws.Server({ port: this.port });
+        this.server = new WSServer({ port: this.port });
+        this.server.on('connection', this.handleConnection.bind(this));
 
-        server.on('connection', (socket, request) => {
-            let mappedAddress = request.socket.remoteAddress!,
-                [, remoteAddress] = mappedAddress.split('::ffff:');
+        // this.server.on('connection', (socket: ws, request: http.ServerResponse) => {
+        //     // let mappedAddress = request.socket?.remoteAddress!,
+        //     //     [, remoteAddress] = mappedAddress.split('::ffff:');
 
-            socket.conn = { remoteAddress };
+        //     // socket.conn = { remoteAddress };
 
-            log.info(`Received connection from: ${socket.conn.remoteAddress}.`);
+        //     // log.info(`Received connection from: ${socket.conn.remoteAddress}.`);
 
-            // TODO - Handle client version....
+        //     // // TODO - Handle client version....
 
-            let connection = new Connection(
-                Utils.createInstance(Modules.EntityType.Player),
-                this.type,
-                socket as AnySocket,
-                this.socketHandler
-            );
+        //     // let connection = new Connection(
+        //     //     Utils.createInstance(Modules.EntityType.Player),
+        //     //     this.type,
+        //     //     socket as AnySocket,
+        //     //     this.socketHandler
+        //     // );
 
-            this.addCallback?.(connection);
-        });
+        //     // this.addCallback?.(connection);
+        // });
+    }
 
-        this.server = server;
+    /**
+     * We handle each connection individually and add it to our list.
+     * @param socket The socket connection we have just received.
+     * @param request An http request since WebSocket protocol is layered on top of TCP.
+     */
+
+    private handleConnection(socket: AnySocket, request: http.ServerResponse): void {
+        let rawAddress = request.socket?.remoteAddress, //Raw ipv6 address.
+            remoteAddress = rawAddress!.replace('::ffff:', '');
+
+        log.info(`Received connection from: ${remoteAddress}.`);
+
+        let id = Utils.createInstance(Modules.EntityType.Player),
+            connection = new Connection(id, this.type, remoteAddress, socket, this.socketHandler);
+
+        this.addCallback?.(connection);
     }
 }
