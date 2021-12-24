99ed198285347086ae2ff6fd65105c71f8ab1cdd
Flavius Poenaru <Flavius256@icloud.com>
2021-12-24 01:32:24 -0700

    Cleaned and documented `entities` class

--- packages/server/src/controllers/entities.ts
+++ packages/server/src/controllers/entities.ts
@@ -45,6 +45,11 @@ export default class Entities {
         this.load();
     }
 
+    /**
+     * Parse through all the statically spawned entities in the map
+     * and create them based on their type.
+     */
+
     private load(): void {
         this.map.forEachEntity((position: Position, key: string) => {
             let type = this.getEntityType(key);
@@ -142,10 +147,11 @@ export default class Entities {
 
         return projectile;
     }
-
     /**
-     * Add Entities
+     * Adds an entity to the list of entities. Whether it be a mob, player
+     * npc, item, chest, etc. we keep track of them in the list of entities.
      */
+
     private add(entity: Entity): void {
         if (entity.instance in this.entities)
             log.warning(`Entity ${entity.instance} already exists.`);
@@ -155,58 +161,45 @@ export default class Entities {
         this.regions.handle(entity);
 
         this.grids.addToEntityGrid(entity, entity.x, entity.y);
-
-        // // Todo move this into a separate handler.
-        // entity.onMovement(() => {
-        //     this.grids.updateEntityPosition(entity);
-
-        //     if (!entity.isMob()) return;
-
-        //     if (!entity.isOutsideSpawn()) return;
-
-        //     entity.removeTarget();
-
-        //     entity.combat.forget();
-        //     entity.combat.stop();
-
-        //     entity.return();
-
-        //     this.world.push(Opcodes.Push.Broadcast, [
-        //         {
-        //             message: new Messages.Combat(Opcodes.Combat.Finish, {
-        //                 attackerId: null,
-        //                 targetId: entity.instance
-        //             })
-        //         },
-        //         {
-        //             message: new Messages.Movement(Opcodes.Movement.Move, {
-        //                 id: entity.instance,
-        //                 x: entity.x,
-        //                 y: entity.y,
-        //                 forced: false,
-        //                 teleport: false
-        //             })
-        //         }
-        //     ]);
-        // });
-
-        if (entity instanceof Character)
-            entity.onStunned((stun: boolean) => {
-                this.world.push(Opcodes.Push.Regions, {
-                    regionId: entity.region,
-                    message: new Messages.Movement(Opcodes.Movement.Stunned, {
-                        id: entity.instance,
-                        state: stun
-                    })
-                });
-            });
     }
+
+    /**
+     * Removes the entity from the entity dictionary and sends a despawn
+     * callback to the nearby regions the entity is in.
+     */
+
+    public remove(entity: Entity): void {
+        this.world.push(Opcodes.Push.Regions, {
+            regionId: entity.region,
+            message: new Messages.Despawn(entity.instance)
+        });
+
+        // Remove the entity from the entity grid
+        this.grids.removeFromEntityGrid(entity, entity.x, entity.y);
+
+        // Remove the entity from the region it is in.
+        this.regions.remove(entity);
+
+        delete this.entities[entity.instance];
+
+        if (entity.isPlayer()) this.world.cleanCombat(entity as Character);
+    }
+
+    /**
+     * Adds the NPC to our dictionary of NPCs.
+     * @param npc NPC we are adding.
+     */
 
     private addNPC(npc: NPC): void {
         this.add(npc);
 
         this.npcs[npc.instance] = npc;
     }
+
+    /**
+     * Adds the item to our dictionary of items.
+     * @param item Item we are adding.
+     */
 
     private addItem(item: Item): void {
         if (item.respawnable) item.onRespawn(() => this.addItem(item));
@@ -216,19 +209,24 @@ export default class Entities {
         this.items[item.instance] = item;
     }
 
+    /**
+     * Adds the mob instance to its dictionary and its
+     * chest area if existent.
+     * @param mob Mob instance we are adding.
+     */
+
     public addMob(mob: Mob): void {
         this.add(mob);
 
         this.mobs[mob.instance] = mob;
 
         mob.addToChestArea(this.map.getChestAreas());
-
-        mob.onHit((attacker: Character) => {
-            if (mob.isDead() || mob.combat.started) return;
-
-            mob.combat.begin(attacker);
-        });
     }
+
+    /**
+     * Adds the newly created player instance to our list of players.
+     * @param player Player we are adding to our dictionary.
+     */
 
     public addPlayer(player: Player): void {
         this.add(player);
@@ -238,11 +236,21 @@ export default class Entities {
         this.world.populationCallback?.();
     }
 
+    /**
+     * Creates a chest and adds it to its dictionary.
+     * @param chest Chest we are adding to our dictionary.
+     */
+
     private addChest(chest: Chest): void {
         this.add(chest);
 
         this.chests[chest.instance] = chest;
     }
+
+    /**
+     * Creates a projectile and adds it to its dictionary.
+     * @param projectile Projectile we are adding to our dictionary.
+     */
 
     private addProjectile(projectile: Projectile): void {
         this.add(projectile);
@@ -251,56 +259,56 @@ export default class Entities {
     }
 
     /**
-     * Remove Entities
+     * Removes an item from the items dictionary and only
+     * respawns it if it's a statically spawned item.
+     * @param item Item we are removing.
      */
-    public remove(entity: Entity): void {
-        this.grids.removeFromEntityGrid(entity, entity.x, entity.y);
-
-        this.regions.remove(entity);
-
-        delete this.entities[entity.instance];
-        delete this.mobs[entity.instance];
-        delete this.items[entity.instance];
-        delete this.players[entity.instance];
-        delete this.projectiles[entity.instance];
-    }
 
     public removeItem(item: Item): void {
         this.remove(item);
 
-        this.world.push(Opcodes.Push.Broadcast, {
-            message: new Messages.Despawn(item.instance)
-        });
-
         if (item.respawnable) item.respawn();
+        else delete this.items[item.instance];
     }
 
-    public removePlayer(player: Player): void {
-        this.world.push(Opcodes.Push.Regions, {
-            regionId: player.region,
-            message: new Messages.Despawn(player.instance)
-        });
+    /**
+     * Removes the mob from our mob dictionary.
+     * @param mob Mob we are removing.
+     */
 
+    public removeMob(mob: Mob): void {
+        this.remove(mob);
+
+        delete this.mobs[mob.instance];
+    }
+
+    /**
+     * Removes the player and clears out its packets and instance.
+     * @param player Player we are removing.
+     */
+
+    public removePlayer(player: Player): void {
         this.remove(player);
 
         if (player.ready) player.save();
 
         this.world.populationCallback?.();
 
-        this.world.cleanCombat(player);
-
+        delete this.players[player.instance];
         delete this.world.network.packets[player.instance];
 
         // Unsure about this since garbage collector should handle it.
         player.destroy();
     }
 
+    /**
+     * Removes a chest or respawns it if it's a statically
+     * spawned chest.
+     * @param chest Chest we are removing.
+     */
+
     public removeChest(chest: Chest): void {
         this.remove(chest);
-
-        this.world.push(Opcodes.Push.Broadcast, {
-            message: new Messages.Despawn(chest.instance)
-        });
 
         if (chest.static) chest.respawn();
         else delete this.chests[chest.instance];
@@ -309,6 +317,7 @@ export default class Entities {
     /**
      * Getters
      */
+
     public isOnline(username: string): boolean {
         return !!this.getPlayer(username);
     }
