1d63235066115e8f219863352bc56463b357e8a0
Flavius Poenaru <Flavius256@icloud.com>
2021-12-28 03:05:04 -0700

    Creator/loader, equipment refactor save/load start

---
+++ packages/server/src/game/entity/character/player/equipment/equipments.ts
@@ -0,0 +1,159 @@
+import _ from 'lodash';
+
+import log from '@kaetram/common/util/log';
+import { Modules } from '@kaetram/common/network';
+import { SEquipment } from '@kaetram/common/types/equipment';
+
+import Player from '../player';
+
+import Armour from './impl/armour';
+import Boots from './impl/boots';
+import Equipment from './impl/equipment';
+import Pendant from './impl/pendant';
+import Ring from './impl/ring';
+import Weapon from './impl/weapon';
+
+export default class Equipments {
+    private armour: Armour = new Armour(-1);
+    private boots: Boots = new Boots(-1);
+    private pendant: Pendant = new Pendant(-1);
+    private ring: Ring = new Ring(-1);
+    private weapon: Weapon = new Weapon(-1);
+
+    // Store all equipments for parsing.
+    // Make sure these are in the order of the enum.
+    private equipments: Equipment[] = [
+        this.armour,
+        this.boots,
+        this.pendant,
+        this.ring,
+        this.weapon
+    ];
+
+    private loadCallback?: () => void;
+
+    public constructor(private player: Player) {}
+
+    /**
+     * Takes the equipment data stored in the database and 'de-serializes' it
+     * by updating each individual equipment.
+     * @param equipmentInfo The information about equipments from the database.
+     */
+
+    public load(equipmentInfo: SEquipment[]): void {
+        _.each(equipmentInfo, (info: SEquipment) => {
+            let equipment = this.getEquipment(info.type);
+
+            if (!equipment) return;
+            if (info.id === -1) return; // Skip if the item is already null
+
+            equipment.update(info.id, info.count, info.ability, info.abilityLevel);
+        });
+
+        this.loadCallback?.();
+    }
+
+    /**
+     * Takes information about an item and equips it onto the player. It figures
+     * out what equipment type it is, and updates that equipment's information.
+     */
+
+    public equip(id: number, count: number, ability: number, abilityLevel: number): void {
+        log.warning('equip not implemented.');
+    }
+
+    /**
+     * Each equipment is organized in the same order as the `Modules.Equipment`
+     * enum. As such, we use the type to pick from the array. We must make sure
+     * that any new equipments that are added have to follow the SAME order
+     * as the enumeration.
+     * @param type The type of equipment from Modules.
+     * @returns The equipment in the index.
+     */
+
+    public getEquipment(type: Modules.Equipment): Equipment {
+        return this.equipments[type];
+    }
+
+    /**
+     * Supplemental getters for more easily accessing equipments.
+     * Instead of having to write `player.equipment.getEquipment(Modules.Equipment.Armour)`
+     * you can just use these getters -> `player.equipment.getArmour()`
+     */
+
+    /**
+     * Grabs the armour equipment of the player.
+     * @returns Armour equipment type.
+     */
+
+    public getArmour(): Armour {
+        return this.getEquipment(Modules.Equipment.Armour);
+    }
+
+    /**
+     * Grabs the boots equipment of the player.
+     * @returns Botos equipment type.
+     */
+
+    public getBoots(): Boots {
+        return this.getEquipment(Modules.Equipment.Boots);
+    }
+
+    /**
+     * Grabs the pendant equipment of the player.
+     * @returns Pendant equipment type.
+     */
+
+    public getPendant(): Pendant {
+        return this.getEquipment(Modules.Equipment.Pendant);
+    }
+
+    /**
+     * Grabs the ring equipment of the player.
+     * @returns Ring equipment type.
+     */
+
+    public getRing(): Ring {
+        return this.getEquipment(Modules.Equipment.Ring);
+    }
+
+    /**
+     * Grabs the weapon equipment of the player.
+     * @returns Weapon equipment type.
+     */
+
+    public getWeapon(): Weapon {
+        return this.getEquipment(Modules.Equipment.Weapon);
+    }
+
+    /**
+     * Goes through each one of our equipments and serializes it. It extracts
+     * cruical information, such as the id, count, ability, and abilityLevel.
+     * @returns A serialized version of the equipment information.
+     */
+
+    public serialize(): SEquipment[] {
+        let equipments: SEquipment[] = [];
+
+        this.forEachEquipment((equipment: Equipment) => equipments.push(equipment.serialize()));
+
+        return equipments;
+    }
+
+    /**
+     * Parses through each equipment in the equipments array.
+     * @param callback Calls back each individual equipment.
+     */
+
+    public forEachEquipment(callback: (equipment: Equipment) => void): void {
+        _.each(this.equipments, callback);
+    }
+
+    /**
+     * Callback signal for when the equipment is loaded.
+     */
+
+    public onLoaded(callback: () => void): void {
+        this.loadCallback = callback;
+    }
+}
---
+++ packages/server/src/game/entity/character/player/equipment/impl/equipment.ts
@@ -0,0 +1,62 @@
+/**
+ * Skeleton for the equipment itself. An equipment
+ * is just an item that is stored in the player's
+ * equipment slot instead of inventory slot.
+ */
+
+import { Modules } from '@kaetram/common/network';
+import { SEquipment } from '@kaetram/common/types/equipment';
+import Items from '@kaetram/server/src/info/items';
+
+export default class Equipment {
+    private updateCallback?: (itemString: string) => void;
+
+    // Basic initialization
+    public constructor(
+        public type: Modules.Equipment,
+        public id: number,
+        public count = 1,
+        public ability = -1,
+        public abilityLevel = -1
+    ) {}
+
+    /**
+     * Updates the equipment with the given item information.
+     * @param id The item id.
+     * @param count The count of items (if we are wearing arrows)
+     * @param ability Ability type of the item.
+     * @param abilityLevel Ability level of the item.
+     */
+
+    public update(id: number, count = 1, ability = -1, abilityLevel = -1): void {
+        this.id = id;
+        this.count = count;
+        this.ability = ability;
+        this.abilityLevel = abilityLevel;
+
+        this.updateCallback?.(this.id === -1 ? 'null' : Items.idToString(this.id));
+    }
+
+    /**
+     * Serializes the information about the equipment and returns it in the format
+     * of the SEquipemnt interface object.
+     * @returns An SEquipment object containing the id, count, ability, and abilityLevel
+     */
+
+    public serialize(): SEquipment {
+        let { type, id, count, ability, abilityLevel } = this;
+
+        return {
+            type,
+            id,
+            count,
+            ability,
+            abilityLevel
+        };
+    }
+
+    // Callback for when the currently equipped item is updated.
+    public onUpdate(callback: (itemString: string) => void): void {
+        this.updateCallback = callback;
+    }
+}
