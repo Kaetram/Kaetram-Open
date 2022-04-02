9ec8875b4ab438c1c9f90123d2e14a0582584c5e
Flavius Poenaru <45127244+Veradictus@users.noreply.github.com>
2022-04-02 12:05:09 -0600

    Hub refactor start
    Former-commit-id: fdba509b27102b2d445d9719a72214165b9ac201

--- packages/hub/src/main.ts
+++ packages/hub/src/main.ts
@@ -8,89 +8,101 @@ import API from './network/api';
 import Discord from './network/discord';
 import { formatServerName } from './util/utils';
 
-class Main {
-    private serversController = new Servers();
-    private api = new API(this.serversController);
-    private database = new Database().getDatabase();
-
-    private discord = new Discord(this.api);
-
-    //private guilds = new Guilds(this.api, this.database);
+export default class Main {
+    private servers: Servers = new Servers();
+    private api: API = new API(this.servers);
+    private discord: Discord = new Discord(this.api);
 
     public constructor() {
-        log.notice(`Initializing ${config.name} game engine...`);
-
-        this.api.setDiscord(this.discord);
-
-        this.load();
-        this.loadConsole();
-    }
-
-    private load() {
-        this.serversController.onAdd((serverId) => {
-            let serverName = formatServerName(serverId);
-
-            log.notice(`Server ${serverId} has been added to the hub.`);
-
-            this.discord.sendRawWebhook(`:white_check_mark: **${serverName} is now online!**`);
-        });
-
-        this.serversController.onRemove((serverId) => {
-            let serverName = formatServerName(serverId);
-
-            log.error(`Server ${serverId} has been removed from hub for inactivity.`);
-
-            this.discord.sendRawWebhook(`:octagonal_sign: **${serverName} has gone offline!**`);
-        });
-    }
-
-    private loadConsole() {
-        let stdin = process.openStdin();
-
-        stdin.addListener('data', (data) => {
-            let message = data.toString().replace(/(\r\n|\n|\r)/gm, ''),
-                type = message.charAt(0);
-
-            if (type !== '/') return;
-
-            let blocks = message.slice(1).split(' '),
-                command = blocks.shift();
-
-            if (!command) return;
-
-            switch (command) {
-                case 'server':
-                    this.api.findEmptyServer((response) => {
-                        console.log(response);
-                    });
-
-                    break;
-
-                case 'player': {
-                    let username = blocks.join(' ');
-
-                    if (!username) {
-                        log.warning('Malformed command - Format: /player [username]');
-                        return;
-                    }
-
-                    this.api.searchForPlayer(username, (response) => {
-                        console.log(response);
-                    });
-
-                    break;
-                }
-
-                case 'guilds': {
-                    this.database.loader.getGuilds().then((guilds) => {
-                        console.log(guilds);
-                    });
-
-                    break;
-                }
-            }
-        });
+        log.notice(`Initializing ${config.name} Hub v${config.gver}.`);
     }
 }
 
-export default new Main();
+new Main();
+
+// class Main {
+//     private serversController = new Servers();
+//     private api = new API(this.serversController);
+//     private database = new Database().getDatabase();
+
+//     private discord = new Discord(this.api);
+
+//     //private guilds = new Guilds(this.api, this.database);
+
+//     public constructor() {
+//         log.notice(`Initializing ${config.name} game engine...`);
+
+//         this.api.setDiscord(this.discord);
+
+//         this.load();
+//         this.loadConsole();
+//     }
+
+//     private load() {
+//         this.serversController.onAdd((serverId) => {
+//             let serverName = formatServerName(serverId);
+
+//             log.notice(`Server ${serverId} has been added to the hub.`);
+
+//             this.discord.sendRawWebhook(`:white_check_mark: **${serverName} is now online!**`);
+//         });
+
+//         this.serversController.onRemove((serverId) => {
+//             let serverName = formatServerName(serverId);
+
+//             log.error(`Server ${serverId} has been removed from hub for inactivity.`);
+
+//             this.discord.sendRawWebhook(`:octagonal_sign: **${serverName} has gone offline!**`);
+//         });
+//     }
+
+//     private loadConsole() {
+//         let stdin = process.openStdin();
+
+//         stdin.addListener('data', (data) => {
+//             let message = data.toString().replace(/(\r\n|\n|\r)/gm, ''),
+//                 type = message.charAt(0);
+
+//             if (type !== '/') return;
+
+//             let blocks = message.slice(1).split(' '),
+//                 command = blocks.shift();
+
+//             if (!command) return;
+
+//             switch (command) {
+//                 case 'server':
+//                     this.api.findEmptyServer((response) => {
+//                         console.log(response);
+//                     });
+
+//                     break;
+
+//                 case 'player': {
+//                     let username = blocks.join(' ');
+
+//                     if (!username) {
+//                         log.warning('Malformed command - Format: /player [username]');
+//                         return;
+//                     }
+
+//                     this.api.searchForPlayer(username, (response) => {
+//                         console.log(response);
+//                     });
+
+//                     break;
+//                 }
+
+//                 case 'guilds': {
+//                     this.database.loader.getGuilds().then((guilds) => {
+//                         console.log(guilds);
+//                     });
+
+//                     break;
+//                 }
+//             }
+//         });
+//     }
+// }
+
+// export default new Main();
