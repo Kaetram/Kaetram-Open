e6e345eb40345a9dd5508fb77e012a5cfdfef016
Flavius Poenaru <Flavius256@icloud.com>
2022-05-04 11:26:40 -0600

    Skill serializing and data types

--- packages/server/src/game/entity/character/player/skills.ts
+++ packages/server/src/game/entity/character/player/skills.ts
@@ -1,6 +1,8 @@
 import Player from './player';
 import Skill from './skill/skill';
 import Lumberjacking from './skill/impl/lumberjacking';
+import { SerializedSkills, SkillData } from '@kaetram/common/types/skills';
+import _ from 'lodash';
 
 export default class Skills {
     private lumberjacking: Lumberjacking = new Lumberjacking();
@@ -8,4 +10,29 @@ export default class Skills {
     private skills: Skill[] = [this.lumberjacking];
 
     public constructor(private player: Player) {}
+
+    /**
+     * Iterates through all the skills and serializes their data.
+     * The data is stored in an array so that it can be parsed.
+     * @returns Array containing data for each skill (at each index).
+     */
+
+    public serialize(): SerializedSkills {
+        let skills: SkillData[] = [];
+
+        this.forEachSkill((skill: Skill) => skills.push(skill.serialize()));
+
+        return {
+            skills
+        };
+    }
+
+    /**
+     * Iterates through all the skills and creates a callback.
+     * @param callback Contains skill being iterated currently.
+     */
+
+    private forEachSkill(callback: (skill: Skill) => void): void {
+        _.each(this.skills, callback);
+    }
 }
