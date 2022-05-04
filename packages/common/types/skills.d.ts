76c24fa88e33ef4b3febf3afa6f9ecd54a35e11f
Flavius Poenaru <Flavius256@icloud.com>
2022-05-04 11:26:40 -0600

    Skill serializing and data types
    Former-commit-id: 7e26c74d372054bd71af533bbe83c685bb9ad082

--- packages/common/types/skills.d.ts
+++ packages/common/types/skills.d.ts
@@ -3,5 +3,9 @@ import Modules from '../network/modules';
 export interface SkillData {
     type: Modules.Skills;
     experience: number;
-    level: number;
+    level?: number;
+}
+
+export interface SerializedSkills {
+    skills: SkillData[];
 }
