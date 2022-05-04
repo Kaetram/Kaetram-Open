c88d2767db9966ddf5366b5a047237956f711e11
Flavius Poenaru <Flavius256@icloud.com>
2022-05-04 11:26:28 -0600

    Database refactor and skill saving/loading

--- packages/server/src/database/mongodb/loader.ts
+++ packages/server/src/database/mongodb/loader.ts
@@ -4,10 +4,34 @@ import type { Db } from 'mongodb';
 import type { EquipmentData, SerializedEquipment } from '@kaetram/common/types/equipment';
 import type { SerializedContainer, SlotData } from '@kaetram/common/types/slot';
 import type Player from '../../game/entity/character/player/player';
-import { QuestData, SerializedQuest } from '@kaetram/common/types/quest';
+import type { QuestData, SerializedQuest } from '@kaetram/common/types/quest';
+import type { SerializedSkills } from '@kaetram/common/types/skills';
 
 export default class Loader {
     public constructor(private database: Db) {}
+
+    /**
+     * Generalized function for loading data from the database. It will return a
+     * data type that is later parsed by the caller. We use this since most serialized
+     * information in Kaetram is stored in a similar fashion, thus cleaning up the code.
+     * @param username The username we are searching for in the collection.
+     * @param collection The name of the collection.
+     * @param callback Raw data from the database.
+     */
+
+    public load(username: string, collection: string, callback: (data: never) => void): void {
+        let cursor = this.database.collection(collection).find({ username });
+
+        cursor.toArray().then((info) => {
+            if (info.length > 1) log.warning(`[${collection}] Duplicate entry for ${username}.`);
+
+            // Return empty array if we can't find any data.
+            if (info.length === 0) return callback([] as never);
+
+            // Return the raw data from the database.
+            callback(info as never);
+        });
+    }
 
     /**
      * Grabs the player equipment system from the database.
@@ -16,17 +40,8 @@ export default class Loader {
      */
 
     public loadEquipment(player: Player, callback: (equipmentInfo: EquipmentData[]) => void): void {
-        let cursor = this.database
-            .collection<SerializedEquipment>('player_equipment')
-            .find({ username: player.username });
-
-        cursor.toArray().then((info) => {
-            if (info.length > 1)
-                log.warning(`[player_equipment] Duplicate entry for ${player.username}.`);
-
-            if (info.length === 0) return callback([]);
-
-            let [{ equipments }] = info;
+        this.load(player.username, 'player_equipment', (info: unknown) => {
+            let [{ equipments }] = info as SerializedEquipment[];
 
             callback(equipments);
         });
@@ -39,17 +54,8 @@ export default class Loader {
      */
 
     public loadInventory(player: Player, callback: (inventoryData: SlotData[]) => void): void {
-        let cursor = this.database
-            .collection<SerializedContainer>('player_inventory')
-            .find({ username: player.username });
-
-        cursor.toArray().then((info) => {
-            if (info.length > 1)
-                log.warning(`[player_inventory] Duplicate entry for ${player.username}.`);
-
-            if (info.length === 0) return callback([]);
-
-            let [{ slots }] = info;
+        this.load(player.username, 'player_inventory', (info: unknown) => {
+            let [{ slots }] = info as SerializedContainer[];
 
             callback(slots);
         });
@@ -62,17 +68,8 @@ export default class Loader {
      */
 
     public loadBank(player: Player, callback: (inventoryData: SlotData[]) => void): void {
-        let cursor = this.database
-            .collection<SerializedContainer>('player_bank')
-            .find({ username: player.username });
-
-        cursor.toArray().then((info) => {
-            if (info.length > 1)
-                log.warning(`[player_bank] Duplicate entry for ${player.username}.`);
-
-            if (info.length === 0) return callback([]);
-
-            let [{ slots }] = info;
+        this.load(player.username, 'player_bank', (info: unknown) => {
+            let [{ slots }] = info as SerializedContainer[];
 
             callback(slots);
         });
@@ -85,19 +82,18 @@ export default class Loader {
      */
 
     public loadQuests(player: Player, callback: (questData: QuestData[]) => void): void {
-        let cursor = this.database
-            .collection<SerializedQuest>('player_quests')
-            .find({ username: player.username });
-
-        cursor.toArray().then((info) => {
-            if (info.length > 1)
-                log.warning(`[player_quests] Duplicate entry for ${player.username}.`);
-
-            if (info.length === 0) return callback([]);
-
-            let [{ quests }] = info;
+        this.load(player.username, 'player_quests', (info: unknown) => {
+            let [{ quests }] = info as SerializedQuest[];
 
             callback(quests);
+        });
+    }
+
+    public loadSkills(player: Player, callback: (skills: unknown) => void): void {
+        this.load(player.username, 'player_skills', (info: unknown) => {
+            let [{ skills }] = info as SerializedSkills[];
+
+            callback(skills);
         });
     }
 }
