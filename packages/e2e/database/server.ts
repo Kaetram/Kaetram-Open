80794ed4eda5658a002311bf4fcc1764e446212d
Lemuel DLS <lemueld73@gmail.com>
2022-07-19 04:08:32 -0400

    🔧 Update database config

--- packages/e2e/database/server.ts
+++ packages/e2e/database/server.ts
@@ -4,13 +4,13 @@ import expressMongoRest from 'express-mongo-rest';
 
 let app = express();
 
-const router = expressMongoRest('mongodb://localroot:localpass@localhost:27017/kaetram_e2e');
+const router = expressMongoRest('mongodb://localhost:27017/kaetram_e2e');
 app.use('/api/v1', router);
 
 router.get('/health', (req, res) => {
     res.status(200).send('Ok');
 });
 
-let server = app.listen(3000, function () {
+let server = app.listen(3000, () => {
     console.log('Listening on Port', server.address());
 });
