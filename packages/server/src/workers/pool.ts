2f078978ee46ad3325040d079b48a3f539ea5459
Flavius Poenaru <45127244+Veradictus@users.noreply.github.com>
2022-08-03 13:41:28 -0600

    Worker pool prototype
    Former-commit-id: eb0c8f6c0c97c7942726f3ef540e9722dd01b5d0

---
+++ packages/server/src/workers/pool.ts
@@ -0,0 +1,25 @@
+import { Worker } from 'worker_threads';
+
+import log from '@kaetram/common/util/log';
+
+export default class Pool {
+    private workers: { [id: number]: Worker } = {};
+
+    public constructor(threads: number) {
+        for (let i = 0; i < threads; i++) {
+            let worker = new Worker('./src/workers/worker.js');
+
+            this.workers[worker.threadId] = worker;
+
+            worker.on('message', (message) => {
+                log.info(`Worker ${worker.threadId} says: ${message.test}`);
+            });
+
+            worker.on('online', () => {
+                worker.postMessage(`Hello from worker ${worker.threadId}!`);
+            });
+        }
+
+        log.info(`Created ${threads} worker threads.`);
+    }
+}
