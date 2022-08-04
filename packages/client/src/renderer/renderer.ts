c3de62de45a5418accff0296edcd89ecddd95c99
Flavius Poenaru <45127244+Veradictus@users.noreply.github.com>
2022-08-04 01:38:41 -0600

    Refactor animations
    Former-commit-id: 2ea095b60064e13e499b3da86cd6aeaf5fb1e978

--- packages/client/src/renderer/renderer.ts
+++ packages/client/src/renderer/renderer.ts
@@ -670,7 +670,7 @@ export default class Renderer {
      */
 
     private drawEntity(entity: Entity): void {
-        let frame = entity.animation?.currentFrame,
+        let frame = entity.animation?.frame,
             dx = entity.x * this.camera.zoomFactor,
             dy = entity.y * this.camera.zoomFactor,
             flipX = dx + this.actualTileSize,
@@ -776,13 +776,13 @@ export default class Renderer {
 
         let animation = player.animation!,
             weaponAnimationData = weapon.animationData[animation.name],
-            frame = animation.currentFrame,
+            { frame, row } = animation,
             index =
                 frame.index < weaponAnimationData.length
                     ? frame.index
                     : frame.index % weaponAnimationData.length,
             weaponX = weapon.width * index,
-            weaponY = weapon.height * animation.row,
+            weaponY = weapon.height * row,
             weaponWidth = weapon.width,
             weaponHeight = weapon.height;
 
@@ -812,7 +812,7 @@ export default class Renderer {
         if (!sprite.loaded) sprite.load();
 
         let animation = character.getEffectAnimation()!,
-            { index } = animation.currentFrame,
+            { index } = animation.frame,
             x = sprite.width * index,
             y = sprite.height * animation.row;
 
@@ -838,7 +838,7 @@ export default class Renderer {
 
     private drawSparks(): void {
         let { sparksAnimation } = this.game.entities.sprites,
-            sparksFrame = sparksAnimation.currentFrame;
+            sparksFrame = sparksAnimation.frame;
 
         this.entitiesContext.drawImage(
             this.sparksSprite.image,
