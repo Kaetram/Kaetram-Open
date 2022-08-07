f4daee44468bdbab81953679d5f69cd76a548c49
Flavius Poenaru <Flavius256@icloud.com>
2022-08-07 16:38:12 -0600

    Boss plugin
    Former-commit-id: cd041642c49b380f5cb3ee9f68ccd471b9266a7d

--- packages/server/data/plugins/mobs/ogrelord.ts
+++ packages/server/data/plugins/mobs/ogrelord.ts
@@ -1,6 +1,9 @@
+import _ from 'lodash';
+
 import Default from './default';
 
 import Mob from '@kaetram/server/src/game/entity/character/mob/mob';
+import Character from '@kaetram/server/src/game/entity/character/character';
 
 import Utils from '@kaetram/common/util/utils';
 
@@ -10,11 +13,116 @@ export default class OgreLord extends Default {
         'No, do not touch my onions!'
     ];
 
+    private minions: Mob[] = [];
+    private positions: Position[] = [
+        { x: 240, y: 65 },
+        { x: 248, y: 65 },
+        { x: 240, y: 70 },
+        { x: 248, y: 70 }
+    ];
+
+    // Waves of minions the boss spawns.
+    private firstWaveMinions = false;
+    private secondWaveMinions = false;
+
+    // Minion keys for waves
+    private minionKeys = ['ogre', 'ironogre'];
+
     public constructor(mob: Mob) {
         super(mob);
 
         // Start the random dialogue interval and run it every 15 seconds.
         setInterval(() => this.randomDialogue(), 15_000);
+    }
+
+    protected override handleHit(damage: number, attacker?: Character): void {
+        super.handleHit(damage, attacker);
+
+        this.handleMinions();
+    }
+
+    /**
+     * Override for the respawn function. We reset the boss back
+     * to default status and remove all the minion wave checks.
+     */
+
+    protected override handleDeath(attacker?: Character): void {
+        super.handleDeath(attacker);
+
+        this.despawn();
+
+        this.firstWaveMinions = false;
+        this.secondWaveMinions = false;
+    }
+
+    /**
+     * Handles the logic checking for minion spawning and creates them.
+     */
+
+    private handleMinions(): void {
+        if (this.isHalfHealth() && !this.firstWaveMinions) this.spawn(0);
+        else if (this.isQuarterHealth() && !this.secondWaveMinions) this.spawn(1);
+    }
+
+    /**
+     * Spawn a minion at each of the respective positions specified. We pick the
+     * minion key depending on the wave we're currently on.
+     * @param wave The wave of minions to spawn.
+     */
+
+    private spawn(wave: number): void {
+        let key = this.minionKeys[wave];
+
+        // Iterate through the positions and spawn a mob at each one.
+        _.each(this.positions, (position: Position) => {
+            let minion = this.world.entities.spawnMob(key, position.x, position.y);
+
+            // Prevent minion from respawning after death.
+            minion.respawnable = false;
+
+            // Add the minion to the list of minions.
+            this.minions.push(minion);
+
+            // Skip if no attackers but somehow the boss got hit.
+            if (this.mob.attackers.length === 0) return;
+
+            // Find an attacker out of the list of attackers.
+            let target = this.mob.attackers[Utils.randomInt(0, this.mob.attackers.length - 1)];
+
+            // Have the minions attack one of the boss' attackers.
+            minion.combat.attack(target);
+        });
+
+        // Prevent the boss from spawning more minions afterwards.
+        if (wave === 0) this.firstWaveMinions = true;
+        else if (wave === 1) this.secondWaveMinions = true;
+
+        // Spawn minions text message.
+        this.mob.talkCallback?.('My minions will surely help defeat you!');
+    }
+
+    /**
+     * Removes all the minions from the world.
+     */
+
+    private despawn(): void {
+        _.each(this.minions, (minion: Mob) => minion.deathCallback?.());
+    }
+
+    /**
+     * @returns Whether or not the boss is halfway through his health.
+     */
+
+    private isHalfHealth(): boolean {
+        return this.mob.hitPoints.getHitPoints() / this.mob.hitPoints.getMaxHitPoints() <= 0.5;
+    }
+
+    /**
+     * @returns Whether or not the boss has one quarter of his life left.
+     */
+
+    private isQuarterHealth(): boolean {
+        return this.mob.hitPoints.getHitPoints() / this.mob.hitPoints.getMaxHitPoints() <= 0.25;
     }
 
     /**
