4d1f0584b07f576eccb4ee8afe0e3ec06f4e6ecd
Flavius Poenaru <Flavius256@icloud.com>
2022-08-29 17:06:37 -0600

    Integrate minion spawning into default class
    Former-commit-id: 31bf6881f4a01ef8f9f1360b4344f75e8a59dca7

--- packages/server/data/plugins/mobs/default.ts
+++ packages/server/data/plugins/mobs/default.ts
@@ -8,7 +8,36 @@ import Handler from '@kaetram/server/src/game/entity/character/mob/handler';
  */
 
 export default class Default extends Handler {
+    // Support for minion spawning. Since a lot of bosses may spawn minions.
+    protected minions: Mob[] = [];
+
     public constructor(mob: Mob) {
         super(mob);
+    }
+
+    /**
+     * Creates a mob given the key and position, and creates the necessary callbacks
+     * and handlers for its death.
+     * @param key The key of the mob to spawn.
+     * @param x The x grid coordinate to spawn the mob at.
+     * @param y The y grid coordinate to spawn the mob at.
+     * @returns The spawned minion object.
+     */
+
+    protected spawn(key: string, x: number, y: number): Mob {
+        let minion = this.world.entities.spawnMob(key, x, y);
+
+        // Prevent minion from respawning after death.
+        minion.respawnable = false;
+
+        // Remove the minion from the list when it dies.
+        minion.onDeathImpl(() => {
+            this.minions.splice(this.minions.indexOf(minion), 1);
+        });
+
+        // Add the minion to the list.
+        this.minions.push(minion);
+
+        return minion;
     }
 }
