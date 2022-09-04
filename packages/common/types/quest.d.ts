ff44483cbafa267055400a7eb4c6a6e46d71c4a0
Flavius Poenaru <45127244+Veradictus@users.noreply.github.com>
2022-09-04 12:36:44 -0600

    Dynamic entity visibility

--- packages/common/types/quest.d.ts
+++ packages/common/types/quest.d.ts
@@ -42,6 +42,7 @@ export interface RawStage {
 export interface RawQuest {
     name: string;
     description: string;
+    hideNPCs: string[]; // NPCs to hide after quest.-
     stages: { [id: number]: RawStage };
 }
 
