8b3a61f3b3ae60d36da39884ab369d3e2e621efa
Tom Vergult <fvantom@terranovita.com>
2022-09-21 20:03:04 +0200

    feat(server): Better roaming for mobs

--- packages/common/network/modules.ts
+++ packages/common/network/modules.ts
@@ -384,7 +384,7 @@ export enum MobDefaults {
     AGGRO_RANGE = 2, // Default aggro range of 2 tiles
     RESPAWN_DELAY = 60_000, // 60 seconds to respawn
     ROAM_DISTANCE = 7, // 7 tiles away from spawn point
-    ROAM_FREQUENCY = 17_000, // Roam interval every 17 seconds
+    ROAM_FREQUENCY = 10_000, // Roam interval every 10 seconds
     DEFENSE_LEVEL = 1,
     ATTACK_LEVEL = 1
 }
