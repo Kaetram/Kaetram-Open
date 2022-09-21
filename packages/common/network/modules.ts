6a12222b9e6263c1d9486b2bc7aa05627af757f6
Tom Vergult <fvantom@terranovita.com>
2022-09-21 20:27:57 +0200

    feat(server): Put amount of retries in MobDefaults
    Former-commit-id: a8369ce38abc8fea5fcdf7878975eefbd75a4cdc

--- packages/common/network/modules.ts
+++ packages/common/network/modules.ts
@@ -385,6 +385,7 @@ export enum MobDefaults {
     RESPAWN_DELAY = 60_000, // 60 seconds to respawn
     ROAM_DISTANCE = 7, // 7 tiles away from spawn point
     ROAM_FREQUENCY = 10_000, // Roam interval every 10 seconds
+    ROAM_RETRIES = 5, // 5 retries if the new spot is not possible
     DEFENSE_LEVEL = 1,
     ATTACK_LEVEL = 1
 }
