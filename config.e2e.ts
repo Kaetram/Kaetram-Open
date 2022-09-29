b3b55f9501dbabb7f075698e2b2eda670ac0491f
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2022-09-29 05:26:54 -0400

    again but better

--- config.e2e.ts
+++ config.e2e.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@kaetram/config';
 
 export default defineConfig({
-    host: '0.0.0.0',
+    host: '127.0.0.1',
     skipDatabase: false,
     mongodbDatabase: 'kaetram_e2e'
 });
