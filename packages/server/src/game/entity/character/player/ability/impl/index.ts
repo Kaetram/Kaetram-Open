246f339c01a3396932e04780cb517f34cd929f29
Flavius Poenaru <Flavius256@icloud.com>
2022-10-03 14:00:12 -0600

    Add special ability skeleton

--- packages/server/src/game/entity/character/player/ability/impl/index.ts
+++ packages/server/src/game/entity/character/player/ability/impl/index.ts
@@ -1,5 +1,17 @@
+import intimidate from './intimidate';
 import run from './run';
+import awareness from './awareness';
+import dualistsmark from './dualistsmark';
+import hotshot from './hotshot';
+import thickskin from './thickskin';
+import precognition from './precognition';
 
 export default {
-    run
+    intimidate,
+    run,
+    awareness,
+    dualistsmark,
+    hotshot,
+    thickskin,
+    precognition
 };
