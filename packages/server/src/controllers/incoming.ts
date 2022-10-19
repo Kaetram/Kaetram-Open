295b6501232a57c52f7d6ca4af20cd8dda5e0b6e
Flavius Poenaru <45127244+Veradictus@users.noreply.github.com>
2022-10-19 12:51:21 -0600

    Equipment screen and stats

--- packages/server/src/controllers/incoming.ts
+++ packages/server/src/controllers/incoming.ts
@@ -158,6 +158,7 @@ export default class Incoming {
         this.player.updateRegion();
         this.player.updateEntities();
         this.player.updateEntityList();
+        this.player.updateExperience();
 
         this.world.api.sendChat(Utils.formatName(this.player.username), 'has logged in!');
         this.world.discord.sendMessage(this.player.username, 'has logged in!');
