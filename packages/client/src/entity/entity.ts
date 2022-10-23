8cc7f3ecb137fe67d045700eac4dfb636fdd2794
Flavius Poenaru <Flavius256@icloud.com>
2022-10-23 12:11:19 -0600

    Prevent projectiles from being added to regions
    Former-commit-id: 9a825d1cb743bbd645ed20cc669912d095267879

--- packages/client/src/entity/entity.ts
+++ packages/client/src/entity/entity.ts
@@ -150,10 +150,7 @@ export default abstract class Entity {
     public setSprite(sprite: Sprite): void {
         if (!sprite || (this.sprite && this.sprite.name === sprite.name)) return;
 
-        if (this.isPlayer()) {
-            sprite.loadHurt = true;
-            this.idle();
-        }
+        if (this.isPlayer()) sprite.loadHurt = true;
 
         if (!sprite.loaded) sprite.load();
 
