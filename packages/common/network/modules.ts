7aa7ba76a93422bfa417c3b6f81b9f078164e092
Flavius Poenaru <45127244+Veradictus@users.noreply.github.com>
2022-10-30 00:47:09 -0600

    Map size 816x528 and region size 48x48
    Former-commit-id: 9c749b41bbd43c55e375e59442aea1602011e5fa

--- packages/common/network/modules.ts
+++ packages/common/network/modules.ts
@@ -367,7 +367,7 @@ export const enum Constants {
     MAX_PROFESSION_LEVEL = 99, // Totally not influenced by another game lol
     HEAL_RATE = 5000, // healing every 5 seconds
     STORE_UPDATE_FREQUENCY = 20_000, // update store every 20 seconds
-    MAP_DIVISION_SIZE = 64, // The size of a region the map is split into.
+    MAP_DIVISION_SIZE = 48, // The size of a region the map is split into.
     SPAWN_POINT = '405,27', // Default starting point outside the tutorial
     TUTORIAL_QUEST_KEY = 'tutorial', // key of the tutorial quest
     TUTORIAL_SPAWN_POINT = '570,11', // 'x,y' values
