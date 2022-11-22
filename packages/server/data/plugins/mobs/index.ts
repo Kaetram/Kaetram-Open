495a5cf786d9be5b493c76c11c5cebd13b83ecd4
Flavius Poenaru <Flavius256@icloud.com>
2022-11-22 16:39:16 -0700

    Queen Ant minions and combat updates

--- packages/server/data/plugins/mobs/index.ts
+++ packages/server/data/plugins/mobs/index.ts
@@ -1,9 +1,11 @@
 import ogrelord from './ogrelord';
 import skeletonking from './skeletonking';
 import queenant from './queenant';
+import workerant from './workerant';
 
 export default {
     ogrelord,
     skeletonking,
-    queenant
+    queenant,
+    workerant
 };
