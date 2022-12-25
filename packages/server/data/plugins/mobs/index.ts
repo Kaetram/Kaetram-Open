9c4740cb808277903375da861935cf8c0dba50c8
Flavius Poenaru <45127244+Veradictus@users.noreply.github.com>
2022-12-25 01:08:28 -0700

    Forest dragon plugin

--- packages/server/data/plugins/mobs/index.ts
+++ packages/server/data/plugins/mobs/index.ts
@@ -2,10 +2,12 @@ import ogrelord from './ogrelord';
 import skeletonking from './skeletonking';
 import queenant from './queenant';
 import ant from './ant';
+import forestdragon from './forestdragon';
 
 export default {
     ogrelord,
     skeletonking,
     queenant,
-    ant
+    ant,
+    forestdragon
 };
