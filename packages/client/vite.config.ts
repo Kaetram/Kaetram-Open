ee250f0dc21faa024aaab41c16fee99288a01300
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2022-12-26 16:25:31 -0500

    📝 Start privacy notice
    Former-commit-id: 804e753c8be70286936bb842cdcaff26f83ed142

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -91,7 +91,11 @@ export default defineConfig(({ mode }) => {
             }),
             legacy(),
             createHtmlPlugin({
-                minify: isProduction && { processScripts: ['application/ld+json'] }
+                minify: isProduction && { processScripts: ['application/ld+json'] },
+                pages: [
+                    { filename: 'index.html', template: 'index.html' },
+                    { filename: 'privacy.html', template: 'privacy.html' }
+                ]
             })
         ],
         build: {
