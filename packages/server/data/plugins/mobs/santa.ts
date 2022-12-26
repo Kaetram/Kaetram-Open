a6d536d06c795a191bf07e7a0000c07ed2a57109
Flavius Poenaru <Flavius256@icloud.com>
2022-12-25 23:11:58 -0700

    Projectiles and minor refactoring
    Former-commit-id: f3d6f3ea43055a15f2063d96ea78ac666892b7c7

---
+++ packages/server/data/plugins/mobs/santa.ts
@@ -0,0 +1,24 @@
+import Default from './default';
+
+import Mob from '@kaetram/server/src/game/entity/character/mob/mob';
+import Utils from '@kaetram/common/util/utils';
+
+export default class Santa extends Default {
+    public constructor(mob: Mob) {
+        super(mob);
+    }
+
+    /**
+     * Every attack we pick a different projectile to throw.
+     */
+
+    protected override handleAttack(): void {
+        super.handleAttack();
+
+        // Generate a random projectile between 1 and 6 and set the projectile name.
+        let projectile = Utils.randomInt(1, 6),
+            name = `projectile-gift${projectile === 1 ? '' : projectile}`;
+
+        this.mob.projectileName = name;
+    }
+}
