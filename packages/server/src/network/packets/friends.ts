f55ce8d4f47dcd1ae7f90dd7f69e722578f1932a
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2023-01-02 17:38:39 -0500

    🚨 Fix linting

--- packages/server/src/network/packets/friends.ts
+++ packages/server/src/network/packets/friends.ts
@@ -1,6 +1,9 @@
+import { Packets } from '@kaetram/common/network';
+
 import Packet from '../packet';
-import { Opcodes, Packets } from '@kaetram/common/network';
-import { FriendsPacket } from '@kaetram/common/types/messages/outgoing';
+
+import type { Opcodes } from '@kaetram/common/network';
+import type { FriendsPacket } from '@kaetram/common/types/messages/outgoing';
 
 export default class Friends extends Packet {
     public constructor(opcode: Opcodes.Friends, data: FriendsPacket) {
