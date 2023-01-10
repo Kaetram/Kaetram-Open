27a3ec5ad2281003cc2a5e5303124fc64bb2ec83
Flavius Poenaru <45127244+Veradictus@users.noreply.github.com>
2023-01-10 14:08:45 -0700

    Add Cyrillic WASD movement
    Former-commit-id: f7c9bac6ec50243d95f0935a096223b06b5a9540

--- packages/client/src/controllers/input.ts
+++ packages/client/src/controllers/input.ts
@@ -170,24 +170,28 @@ export default class InputController {
 
         switch (event.key) {
             case 'w':
+            case 'ц':
             case 'ArrowUp': {
                 this.player.moveUp = true;
                 return;
             }
 
             case 'a':
+            case 'ф':
             case 'ArrowLeft': {
                 this.player.moveLeft = true;
                 return;
             }
 
             case 's':
+            case 'ы':
             case 'ArrowDown': {
                 this.player.moveDown = true;
                 return;
             }
 
             case 'd':
+            case 'в':
             case 'ArrowRight': {
                 this.player.moveRight = true;
                 return;
