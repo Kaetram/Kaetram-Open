2157d281436282e8c0aad1105838b67873b9f4c3
Flavius Poenaru <Flavius256@icloud.com>
2023-01-13 13:57:13 -0700

    Sorcery quest

--- packages/server/src/game/entity/character/player/quest/impl/index.ts
+++ packages/server/src/game/entity/character/player/quest/impl/index.ts
@@ -1,11 +1,13 @@
-import ancientlands from './ancientlands';
-import desertquest from './desertquest';
-import foresting from './foresting';
+import sorcery from './sorcery';
 import tutorial from './tutorial';
+import foresting from './foresting';
+import desertquest from './desertquest';
+import ancientlands from './ancientlands';
 
 export default {
     tutorial,
     foresting,
     desertquest,
-    ancientlands
+    ancientlands,
+    sorcery
 };
