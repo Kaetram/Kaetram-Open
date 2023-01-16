303265738370cb48b274b5d468e4e9cd10f07972
Flavius Poenaru <45127244+Veradictus@users.noreply.github.com>
2023-01-15 19:15:35 -0700

    New items and content
    Former-commit-id: 33d266e99b342537d895e313dc8d5de9f520a7c0

---
+++ packages/server/data/plugins/mobs/piratecaptain.ts
@@ -0,0 +1,11 @@
+import Utils from '@kaetram/common/util/utils';
+
+import Default from './default';
+
+import type Mob from '@kaetram/server/src/game/entity/character/mob/mob';
+
+export default class PirateCaptain extends Default {
+    public constructor(mob: Mob) {
+        super(mob);
+    }
+}
