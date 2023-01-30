6399e4e74d43d44554323e656a572a76b61fcb09
Flavius Poenaru <45127244+Veradictus@users.noreply.github.com>
2023-01-30 00:23:16 -0700

    feat(server): bank max stack size
    Former-commit-id: b3b0c2d0db6e109d8ef66d70c6d2721baa6c604d

--- packages/server/src/game/entity/character/player/containers/impl/bank.ts
+++ packages/server/src/game/entity/character/player/containers/impl/bank.ts
@@ -3,6 +3,8 @@ import Container from '../container';
 import { Modules } from '@kaetram/common/network';
 
 export default class Bank extends Container {
+    protected override ignoreMaxStackSize = true;
+
     public constructor(size: number) {
         super(Modules.ContainerType.Bank, size);
     }
