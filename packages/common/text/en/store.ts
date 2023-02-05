a90b2200daff6bb7e031ead0ab1f92b7575c992e
Flavius Poenaru <Flavius256@icloud.com>
2023-02-05 11:01:04 -0700

    chore: store text update
    Former-commit-id: 54bc65f3395740fde72114c8a79623ee62450518

--- packages/common/text/en/store.ts
+++ packages/common/text/en/store.ts
@@ -11,6 +11,6 @@ export default {
     CANNOT_SELL_ITEM: 'You cannot sell this item.',
     ACTION_STORE_NOT_OPEN: 'Tried to perform an action on a store that is not open.',
     CHEATER: 'You have been flagged for cheating. You cannot purchase anything.',
-    RESTRICTED_STORE: 'Sorry but sales are final and you cannot sell this item.',
+    RESTRICTED_STORE: 'This store does not accept any items.',
     INVALID_ITEM_COUNT: 'You have entered an invalid amount of items.'
 };
