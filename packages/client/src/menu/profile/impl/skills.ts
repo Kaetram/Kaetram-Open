e3ce929e5b0a9e2ce5ba2b6bb54202db8630a42b
Flavius Poenaru <Flavius256@icloud.com>
2023-02-09 10:58:53 -0700

    fix(client): skill syncing
    Former-commit-id: 03aaf4f3b85d3c986a7ba4f2104183b4326d90d6

--- packages/client/src/menu/profile/impl/skills.ts
+++ packages/client/src/menu/profile/impl/skills.ts
@@ -3,11 +3,20 @@ import Menu from '../../menu';
 import _ from 'lodash-es';
 import { Modules } from '@kaetram/common/network';
 
-import type Player from '../../../entity/character/player/player';
 import type Skill from '../../../entity/character/player/skill';
+import type Player from '../../../entity/character/player/player';
+
+interface SkillElement extends HTMLLIElement {
+    name?: string;
+    level?: number;
+    percentage?: number;
+    originalWidth?: number;
+}
 
 export default class Skills extends Menu {
     private list: HTMLUListElement = document.querySelector('#skills-list > ul')!;
+
+    private loaded = false;
 
     public constructor() {
         super('#skills-page');
@@ -19,9 +28,30 @@ export default class Skills extends Menu {
      */
 
     public override synchronize(player: Player): void {
-        this.list.innerHTML = '';
+        _.each(player.skills, (skill: Skill) => {
+            let element = this.get(skill.name);
 
-        _.each(player.skills, (skill: Skill) => this.list.append(this.createElement(skill)));
+            // Update the element if found.
+            if (element) this.update(element, skill);
+            else {
+                // Create the element otherwise...
+                element = this.createElement(skill);
+
+                // Append the element to the list.
+                this.list.append(element);
+            }
+        });
+
+        /**
+         * This is a hack but if I spend anymore time trying to get the
+         * stupid `offsetWidth` to have a value I will go insane. Someone
+         * else please find a better solution.
+         */
+
+        if (!this.loaded) {
+            this.loaded = true;
+            setTimeout(() => this.synchronize(player), 2000);
+        }
     }
 
     /**
@@ -30,8 +60,8 @@ export default class Skills extends Menu {
      */
 
     private createElement(skill: Skill): HTMLLIElement {
-        let element = document.createElement('li'),
-            name = document.createElement('div'),
+        let element: SkillElement = document.createElement('li'),
+            level = document.createElement('div'),
             image = document.createElement('div'),
             experience = document.createElement('div');
 
@@ -39,8 +69,7 @@ export default class Skills extends Menu {
         element.classList.add('skill');
 
         // Update the skill level
-        name.classList.add('skill-level', 'stroke');
-        name.innerHTML = `${skill.level}/${Modules.Constants.MAX_LEVEL}`;
+        level.classList.add('skill-level', 'stroke');
 
         // Load the image based on the skill name.
         image.classList.add('skill-image', `skill-image-${skill.name.toLowerCase()}`);
@@ -48,12 +77,54 @@ export default class Skills extends Menu {
         // Load up the experience bar.
         experience.classList.add('skill-experience');
 
-        // Set the experience bar's width.
-        experience.style.width = `${experience.offsetWidth * skill.percentage}px`;
-
         // Add the image to the element.
-        element.append(name, experience, image);
+        element.append(level, experience, image);
+
+        // Store information about the skill in the element.
+        element.name = skill.name;
+        element.level = skill.level;
+        element.percentage = skill.percentage;
 
         return element;
+    }
+
+    /**
+     * Finds an HTML skill element extension based on the name of the skill.
+     * @param name The name of the skill we are looking for.
+     * @returns A skill element extension if found, otherwise undefined.
+     */
+
+    private get(name: string): SkillElement | undefined {
+        for (let element of this.list.children)
+            if ((element as SkillElement).name === name) return element as SkillElement;
+
+        return undefined;
+    }
+
+    /**
+     * Updates a slot element with the latest level and percentage information.
+     * @param element SkillElement HTML element that we are updating.
+     * @param skill The skill we are using to update the data with.
+     */
+
+    private update(element: SkillElement, skill: Skill): void {
+        // Update the level and experience bar of the skill.
+        let level: HTMLElement = element.querySelector('.skill-level')!,
+            experience: HTMLElement = element.querySelector('.skill-experience')!;
+
+        // Update the skill level
+        level.innerHTML = `${skill.level}/${Modules.Constants.MAX_LEVEL}`;
+
+        if (experience.offsetWidth === 0) return;
+
+        // Store the original width of the experience bar.
+        if (!element.originalWidth) element.originalWidth = experience.offsetWidth;
+
+        // Set the experience bar's width.
+        experience.style.width = `${element.originalWidth * skill.percentage}px`;
+
+        // Update the properties of the element.
+        element.level = skill.level;
+        element.percentage = skill.percentage;
     }
 }
