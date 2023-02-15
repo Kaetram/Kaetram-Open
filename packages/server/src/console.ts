9b93486cf55e05712dd99ef537911f9060e08917
Veradictus <45127244+Veradictus@users.noreply.github.com>
2023-02-14 23:41:42 -0700

    chore: remove debugging
    Former-commit-id: 8b35aa399c8e69df5519cb6872542b62ac3ae588

--- packages/server/src/console.ts
+++ packages/server/src/console.ts
@@ -145,8 +145,6 @@ export default class Console {
                 }
 
                 case 'searchdupes': {
-                    console.log(`logOnly: ${!blocks.shift()}`);
-
                     return this.database.parsePotentialDupes(!blocks.shift());
                 }
             }
