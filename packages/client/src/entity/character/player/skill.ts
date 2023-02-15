cc5ee072409a4e2b43add6be9a50fc3030151756
Veradictus <45127244+Veradictus@users.noreply.github.com>
2023-02-14 21:47:09 -0700

    fix: various minor bugs
    Former-commit-id: 280a4aefe02952f324dd11f09dfcf1ce01031b0a

--- packages/client/src/entity/character/player/skill.ts
+++ packages/client/src/entity/character/player/skill.ts
@@ -3,6 +3,7 @@ import { Modules } from '@kaetram/common/network';
 export default class Skill {
     public name: string;
     public percentage = 0;
+    public nextExperience = 0;
 
     public constructor(public type: Modules.Skills, public experience = 0, public level = 1) {
         this.name = Modules.Skills[type];
@@ -14,8 +15,14 @@ export default class Skill {
      * @param level Level we are updating to.
      */
 
-    public update(experience: number, level: number, percentage: number): void {
+    public update(
+        experience: number,
+        nextExperience: number,
+        level: number,
+        percentage: number
+    ): void {
         this.experience = experience;
+        this.nextExperience = nextExperience;
         this.level = level || this.level;
         this.percentage = percentage || 0;
     }
