39fde21ff871a9be03481605783b3d50580180d8
Flavius Poenaru <Flavius256@icloud.com>
2023-02-23 16:08:03 -0700

    chore(server): stats and formula tweaking

--- packages/server/src/info/formulas.ts
+++ packages/server/src/info/formulas.ts
@@ -75,7 +75,7 @@ export default {
          */
 
         // Linearly increase accuracy based on accuracy bonus, prevent from going over 50.
-        accuracy += accuracyBonus > 50 ? 0.01 : 1 - accuracyBonus / 55;
+        accuracy += accuracyBonus > 50 ? 0.01 : 1 - accuracyBonus / 50;
 
         // Append the accuracy level bonus, we use a 1.75 modifier since skill level matters more.
         accuracy += (Modules.Constants.MAX_LEVEL - accuracyLevel + 1) * 0.01;
@@ -99,21 +99,21 @@ export default {
             }
 
             case Modules.AttackStyle.Stab: {
-                // Rapid attack style increases accuracy by a factor of 0.05;
-                accuracy -= 0.07;
+                // Rapid attack style increases accuracy by a factor of 0.11;
+                accuracy -= 0.11;
                 break;
             }
 
             case Modules.AttackStyle.Crush:
             case Modules.AttackStyle.Chop: {
-                // Rapid attack style increases accuracy by a factor of 0.04
-                accuracy -= 0.04;
+                // Rapid attack style increases accuracy by a factor of 0.07
+                accuracy -= 0.07;
                 break;
             }
 
             case Modules.AttackStyle.Shared: {
-                // Shared attack style increases accuracy by a factor of 0.03
-                accuracy -= 0.03;
+                // Shared attack style increases accuracy by a factor of 0.05
+                accuracy -= 0.05;
                 break;
             }
         }
@@ -214,7 +214,7 @@ export default {
 
         /**
          * If we have a attack style against a defense style that is not the same, then we can
-         * remove the 1/5th of the weight and append the full weight of the attack style to the
+         * remove the 1/3th of the weight and append the full weight of the attack style to the
          * accuracy modifier.
          */
 
