926bf3fb5ba7c0292111ceb6e9b579cf2060c81e
Veradictus <45127244+Veradictus@users.noreply.github.com>
2023-03-20 00:20:10 -0600

    fix: relay
    Former-commit-id: 79f31b1e0f81509809bf0a8b0fc5b2a69b87db9e

--- packages/common/network/impl/relay.ts
+++ packages/common/network/impl/relay.ts
@@ -4,6 +4,6 @@ import { Packets } from '@kaetram/common/network';
 
 export default class Relay extends Packet {
     public constructor(username: string, packet: Packet) {
-        super(Packets.Relay, undefined, [username, ...(packet.serialize() as unknown[])]);
+        super(Packets.Relay, undefined, [username, [...(packet.serialize() as unknown[])]]);
     }
 }
