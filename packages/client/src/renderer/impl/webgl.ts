eaef6e75934a6ade438936770923a81817675068
Flavius Poenaru <Flavius256@icloud.com>
2023-03-29 14:51:37 -0600

    feat(client): create program data

--- packages/client/src/renderer/impl/webgl.ts
+++ packages/client/src/renderer/impl/webgl.ts
@@ -2,7 +2,15 @@ import Renderer from '../renderer';
 import ImageVertex from '../shaders/image.vert';
 import ImageFragment from '../shaders/image.frag';
 
+import TWGL from 'twgl.js';
+
 import type Game from '../../game';
+
+/**
+ * Huge thanks to the developer of `gl-tiled` for the point of reference in
+ * creating a WebGL 2D tilemap renderer.
+ * GitHub Repository: https://github.com/englercj/gl-tiled
+ */
 
 export default class WebGL extends Renderer {
     // Override for context types and use webgl
