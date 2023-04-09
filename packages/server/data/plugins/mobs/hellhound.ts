1a6f33037ffac602e30a5ef52600729932ea8ad9
Veradictus <45127244+Veradictus@users.noreply.github.com>
2023-04-09 15:56:53 -0600

    feat: stats and balancing

---
+++ packages/server/data/plugins/mobs/hellhound.ts
@@ -0,0 +1,9 @@
+import Default from './default';
+
+import type Mob from '@kaetram/server/src/game/entity/character/mob/mob';
+
+export default class Hellhound extends Default {
+    public constructor(mob: Mob) {
+        super(mob);
+    }
+}
