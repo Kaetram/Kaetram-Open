feabce19774fe105ed9a7d331a75026ea3255734
Veradictus <45127244+Veradictus@users.noreply.github.com>
2023-04-11 23:13:27 -0600

    feat(server): multiple item quest requirement

--- packages/common/types/quest.d.ts
+++ packages/common/types/quest.d.ts
@@ -14,10 +14,10 @@ export interface RawStage {
     mobCountRequirement?: number;
 
     /** Item required in the inventory to progress to next stage. */
-    itemRequirement?: string;
+    itemRequirement?: string[];
 
     /** How many of a given item we need to progress. */
-    itemCountRequirement?: number;
+    itemRequirementCount?: number[];
 
     /** Text for the NPC. */
     text?: DialogueItem[];
@@ -60,8 +60,8 @@ export interface StageData {
     npc?: string;
     mob?: string[];
     mobCountRequirement: number; // how many mobs we need to kill to progress
-    itemRequirement?: string;
-    itemCountRequirement?: number; // how many of an item we need for progression
+    itemRequirement?: string[];
+    itemRequirementCount?: number[]; // how many of an item we need for progression
     text?: string[];
     completedText?: string[];
     pointer?: PointerData;
