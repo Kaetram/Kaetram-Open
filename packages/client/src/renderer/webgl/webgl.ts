061f47dfb190d2450d4bca4cf7f71f8f3ce75a48
Veradictus <45127244+Veradictus@users.noreply.github.com>
2023-04-20 14:24:26 -0600

    feat(client): webgl region updating

--- packages/client/src/renderer/webgl/webgl.ts
+++ packages/client/src/renderer/webgl/webgl.ts
@@ -7,7 +7,7 @@ import LayerVertex from '../shaders/layer.vert';
 import LayerFragment from '../shaders/layer.frag';
 
 import type Game from '../../game';
-import type { RotatedTile } from '@kaetram/common/types/map';
+import type { RegionTile, RotatedTile } from '@kaetram/common/types/map';
 
 /**
  * Huge thanks to the developer of `gl-tiled` for the point of reference in
@@ -111,9 +111,6 @@ export default class WebGL extends Renderer {
         this.inverseTileCount[0] = 1 / this.map.width;
         this.inverseTileCount[1] = 1 / this.map.height;
 
-        // Initialize the texture data with the map information we have.
-        this.loadMapTexture();
-
         // Load the buffer data...
         this.forEachDrawingContext((context: WebGLRenderingContext) => {
             this.tilesets[(context.canvas as HTMLCanvasElement).id] = new Tileset(
@@ -133,27 +130,24 @@ export default class WebGL extends Renderer {
     }
 
     /**
-     * The map texture iterates through all the tile elements in the map and
-     * determines (based on the structure of the tile) which layer to place the
-     * tile on. For example, if we have a tile whose value is a number (i.e. 1),
-     * then we place the tile on the first index of the layers array. If we obtain
-     * an array of numbers (i.e. [1, 2]), then the first element is the 0 index
-     * on the layers and the second element is the 1 index on the layers.
+     * The `setTile` function handles information from the map to update a tile
+     * at a specific index. We do this so that we are always in sync with the
+     * information we receive from the server. Instead of loading the entire map
+     * as a batch, we can load the map in chunks and update the map as we receive
+     * information from the server.
+     * @param index The index at which we want to update the tile.
+     * @param data The data we want to update the tile with, may be an array or a number.
      */
 
-    private loadMapTexture(): void {
-        for (let index = 0; index < this.map.data.length; index++) {
-            let info = this.map.data[index];
+    public override setTile(index: number, data: RegionTile): void {
+        // Clear all the tiles of every layer at the specified index.
+        this.clearTile(index);
 
-            // If we find an array tile then we need to iterate through the array and pass the data to the layers.
-            if (Array.isArray(info)) {
-                for (let tile in info) this.addTile(index, info[tile], parseInt(tile));
-                continue;
-            }
+        // Add the tile if it is not an array
+        if (!Array.isArray(data)) return this.addTile(index, data);
 
-            // Otherwise just add the tile to the first layer.
-            this.addTile(index, info);
-        }
+        // If we find an array tile then we need to iterate through the array and pass the data to the layers.
+        for (let tileIndex in data) this.addTile(index, data[tileIndex], parseInt(tileIndex));
     }
 
     /**
@@ -210,6 +204,16 @@ export default class WebGL extends Renderer {
             shader.uniforms.uInverseTilesetTextureSize!,
             this.inverseTilesetTextureSizeBuffer
         );
+    }
+
+    /**
+     * Override for the `bindTileLayers` function. We use this function after we
+     * finish loading regions to signal to the drawing contexts that we need to
+     * re-bind the tile layers with the new texture data.
+     */
+
+    public override bindTileLayers(): void {
+        this.forEachDrawingContext((context: WebGLRenderingContext) => this.bindTexture(context));
     }
 
     /**
@@ -322,6 +326,16 @@ export default class WebGL extends Renderer {
         if (!this.layers[layerIndex]) this.layers[layerIndex] = new Layer(this.map);
 
         this.layers[layerIndex].addTile(index, tile, this.isFlipped(tile as RotatedTile));
+    }
+
+    /**
+     * Used for clearing a tile from all the layers when we want to dynamically
+     * update a piece of the map.
+     * @param index The index at which we are clearing the tile.
+     */
+
+    private clearTile(index: number): void {
+        for (let layer of this.layers) layer.clear(index);
     }
 
     /**
