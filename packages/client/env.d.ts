5f916668ac2b39b9b88be7c718a891ac3383084f
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2023-06-09 14:08:20 -0400

    refactor: client configurations

--- packages/client/env.d.ts
+++ packages/client/env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="astro/client" />
+/// <reference types="astro/client" />
+/// <reference types="vite-plugin-pwa/client" />
+
+import type { env } from './astro.config';
+
+declare global {
+    let globalConfig: typeof env & {
+        version: string;
+        minor: string;
+        port: number;
+        hub: string | false;
+        sentryDsn: string;
+    };
+}
+
+export {};
