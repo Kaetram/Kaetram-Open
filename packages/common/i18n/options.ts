7b7de46bd0ce9bf32f0915e2ebf1980adff5b842
Flavius Poenaru <flavius256@icloud.com>
2023-07-21 22:30:00 -0600

    feat(common): add new languages

--- packages/common/i18n/options.ts
+++ packages/common/i18n/options.ts
@@ -1,36 +1,124 @@
-import TranslationEn from './en/translation';
-import StoreEn from './en/store';
-import MiscEn from './en/misc';
+import CraftingDe from './de/crafting';
+import EnchantDe from './de/enchant';
+import GuildsDe from './de/guilds';
+import ItemDe from './de/item';
+import MiscDe from './de/misc';
+import ResourceDe from './de/resource';
+import StoreDe from './de/store';
+import TranslationDe from './de/translation';
+import WarpsDe from './de/warps';
 import CraftingEn from './en/crafting';
-import WarpsEn from './en/warps';
-import GuildsEn from './en/guilds';
 import EnchantEn from './en/enchant';
-import TranslationRo from './ro/translation';
-import StoreRo from './ro/store';
-import MiscRo from './ro/misc';
+import GuildsEn from './en/guilds';
+import ItemEn from './en/item';
+import MiscEn from './en/misc';
+import ResourceEn from './en/resource';
+import StoreEn from './en/store';
+import TranslationEn from './en/translation';
+import WarpsEn from './en/warps';
+import CraftingEs from './es/crafting';
+import EnchantEs from './es/enchant';
+import GuildsEs from './es/guilds';
+import ItemEs from './es/item';
+import MiscEs from './es/misc';
+import ResourceEs from './es/resource';
+import StoreEs from './es/store';
+import TranslationEs from './es/translation';
+import WarpsEs from './es/warps';
+import CraftingFr from './fr/crafting';
+import EnchantFr from './fr/enchant';
+import GuildsFr from './fr/guilds';
+import ItemFr from './fr/item';
+import MiscFr from './fr/misc';
+import ResourceFr from './fr/resource';
+import StoreFr from './fr/store';
+import TranslationFr from './fr/translation';
+import WarpsFr from './fr/warps';
 import CraftingRo from './ro/crafting';
-import WarpsRo from './ro/warps';
-import GuildsRo from './ro/guilds';
 import EnchantRo from './ro/enchant';
+import GuildsRo from './ro/guilds';
+import ItemRo from './ro/item';
+import MiscRo from './ro/misc';
+import ResourceRo from './ro/resource';
+import StoreRo from './ro/store';
+import TranslationRo from './ro/translation';
+import WarpsRo from './ro/warps';
+import CraftingRu from './ru/crafting';
+import EnchantRu from './ru/enchant';
+import GuildsRu from './ru/guilds';
+import ItemRu from './ru/item';
+import MiscRu from './ru/misc';
+import ResourceRu from './ru/resource';
+import StoreRu from './ru/store';
+import TranslationRu from './ru/translation';
+import WarpsRu from './ru/warps';
 
 export let resources = {
+    de: {
+        crafting: CraftingDe,
+        enchant: EnchantDe,
+        guilds: GuildsDe,
+        item: ItemDe,
+        misc: MiscDe,
+        resource: ResourceDe,
+        store: StoreDe,
+        translation: TranslationDe,
+        warps: WarpsDe
+    },
     en: {
-        translation: TranslationEn,
-        store: StoreEn,
-        misc: MiscEn,
         crafting: CraftingEn,
-        warps: WarpsEn,
+        enchant: EnchantEn,
         guilds: GuildsEn,
-        enchant: EnchantEn
+        item: ItemEn,
+        misc: MiscEn,
+        resource: ResourceEn,
+        store: StoreEn,
+        translation: TranslationEn,
+        warps: WarpsEn
+    },
+    es: {
+        crafting: CraftingEs,
+        enchant: EnchantEs,
+        guilds: GuildsEs,
+        item: ItemEs,
+        misc: MiscEs,
+        resource: ResourceEs,
+        store: StoreEs,
+        translation: TranslationEs,
+        warps: WarpsEs
+    },
+    fr: {
+        crafting: CraftingFr,
+        enchant: EnchantFr,
+        guilds: GuildsFr,
+        item: ItemFr,
+        misc: MiscFr,
+        resource: ResourceFr,
+        store: StoreFr,
+        translation: TranslationFr,
+        warps: WarpsFr
     },
     ro: {
-        translation: TranslationRo,
-        store: StoreRo,
-        misc: MiscRo,
         crafting: CraftingRo,
-        warps: WarpsRo,
+        enchant: EnchantRo,
         guilds: GuildsRo,
-        enchant: EnchantRo
+        item: ItemRo,
+        misc: MiscRo,
+        resource: ResourceRo,
+        store: StoreRo,
+        translation: TranslationRo,
+        warps: WarpsRo
+    },
+    ru: {
+        crafting: CraftingRu,
+        enchant: EnchantRu,
+        guilds: GuildsRu,
+        item: ItemRu,
+        misc: MiscRu,
+        resource: ResourceRu,
+        store: StoreRu,
+        translation: TranslationRu,
+        warps: WarpsRu
     }
 } as const;
 
