7d28cb19fd3cdec7eed056583831a55bffb6f2e5
Sandra del Mar <30502961+SandradelMarSoto@users.noreply.github.com>
2023-07-24 12:15:39 -0600

    Translated resource.ts to spanish

--- packages/common/i18n/es/resource.ts
+++ packages/common/i18n/es/resource.ts
@@ -1,18 +1,18 @@
 export default {
-    MUST_EQUIP_AXE: 'You must equip an axe in order to cut trees.',
-    MUST_EQUIP_PICKAXE: 'You must be wielding a pickaxe in order to mine rocks.',
-    MUST_EQUIP_FISHING: 'You must be wielding a fishing utensil in order to fish.',
-    MUST_EQUIP_FORAGING: 'You require a special tool to forage this',
+    MUST_EQUIP_AXE: 'Debes equipar un hacha para cortar árboles.',
+    MUST_EQUIP_PICKAXE: 'Debes equipar un pico para minar rocas.',
+    MUST_EQUIP_FISHING: 'Debes equipar una caña de pescar para poder pescar.',
+    MUST_EQUIP_FORAGING: 'Necesitas una herramienta especial para forrajear esto.',
 
-    INVALID_LEVEL_LUMBERJACKING: 'You must be level {{level}} in order to cut this tree.',
-    INVALID_LEVEL_MINING: 'You must be level {{level}} in order to mine this rock.',
-    INVALID_LEVEL_FISHING: 'You must be level {{level}} in order to fish here.',
-    INVALID_LEVEL_FORAGING: 'You must be level {{level}} to harvest this resource.',
+    INVALID_LEVEL_LUMBERJACKING: 'Debes tener el nivel {{level}} para cortar este árbol.',
+    INVALID_LEVEL_MINING: 'Debes tener el nivel {{level}} para minar esta roca.',
+    INVALID_LEVEL_FISHING: 'Debes tener el nivel {{level}} para poder pescar aquí.',
+    INVALID_LEVEL_FORAGING: 'Debes tener el nivel {{level}} para recolectar este recurso.',
 
-    UNABLE_TO_INTERACT_LUMBERJACKING: 'You are unable to cut this tree at the moment.',
-    UNABLE_TO_INTERACT_MINING: 'You do not have the necessary knowledge to mine this rock.',
-    UNABLE_TO_INTERACT_FISHING: 'You are unable to fish here at the moment.',
-    UNABLE_TO_INTERACT_FORAGING: 'You are unable to harvest this resource at the moment.',
+    UNABLE_TO_INTERACT_LUMBERJACKING: 'No puedes cortar este árbol en este momento.',
+    UNABLE_TO_INTERACT_MINING: 'No tienes los conocimientos necesarios para minar esta roca.',
+    UNABLE_TO_INTERACT_FISHING: 'No puede pescar aquí en este momento.',
+    UNABLE_TO_INTERACT_FORAGING: 'No puedes recolectar este recurso en este momento.',
 
-    NO_REASON: 'There is no reason for you to cut this tree.'
+    NO_REASON: 'No hay razón para que cortes este árbol.'
 } as const;
