3c9bfa2d049729d1033d967578ce3415ba741a4e
Sandra del Mar <30502961+SandradelMarSoto@users.noreply.github.com>
2023-07-23 21:37:42 -0600

    Translated guilds.ts to spanish

--- packages/common/i18n/es/guilds.ts
+++ packages/common/i18n/es/guilds.ts
@@ -1,12 +1,12 @@
 export default {
-    ALREADY_EXISTS: 'A guild with that name already exists.',
-    ALREADY_IN_GUILD: 'You are already in a guild.',
-    CANNOT_KICK_YOURSELF: 'You cannot kick yourself from the guild.',
-    GUILD_FULL: 'This guild is at maximum capacity.',
-    MUST_FINISH_TUTORIAL_CREATE: 'You must finish the tutorial before creating a guild.',
-    NO_PERMISSION: 'You do not have permission to do that.',
-    NO_PERMISSION_RANK: 'You do not have permission to update the rank of that member.',
-    NOT_ENOUGH_GOLD: 'You need 30,000 gold in your inventory to create a guild.',
-    NOT_ALLOWED_GUESTS_JOIN: 'Guests are not allowed to join guilds.',
-    NOT_ALLOWED_GUESTS_CREATE: 'Guests are not allowed to create guilds.'
+    ALREADY_EXISTS: 'Ya existe un gremio con ese nombre.',
+    ALREADY_IN_GUILD: 'Ya estás en un gremio.',
+    CANNOT_KICK_YOURSELF: 'No puedes expulsarte a ti mismo del gremio.',
+    GUILD_FULL: 'Este gremio está al máximo de su capacidad.',
+    MUST_FINISH_TUTORIAL_CREATE: 'Debes terminar el tutorial antes de poder crear un gremio.',
+    NO_PERMISSION: 'No tienes permiso para hacer eso.',
+    NO_PERMISSION_RANK: 'No tienes permiso para actualizar el rango de ese miembro.',
+    NOT_ENOUGH_GOLD: 'Necesitas 30 000 de oro en tu inventario para crear un gremio.',
+    NOT_ALLOWED_GUESTS_JOIN: 'Los invitados no pueden unirse a los gremios.',
+    NOT_ALLOWED_GUESTS_CREATE: 'Los invitados no pueden crear gremios.'
 } as const;
