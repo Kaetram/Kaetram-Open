e1e80b2ed20448409114f151942ab1ced171e711
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2023-07-28 11:47:56 -0400

    refactor: move `translation.meta` into `game`

---
+++ packages/common/i18n/es/game.ts
@@ -0,0 +1,7 @@
+export default {
+    meta: {
+        name: 'Kaetram',
+        description:
+            'Kaetram es un motor de videojuegos de código abierto creado para ayudar a aquellos interesados en ingresar al ámbito del desarrollo de videojuegos. La idea original se basa en el juego de demostración de Little Workshop: BrowserQuest. Los activos se han mantenido igual, pero el código en sí se ha borrado por completo y se ha rehecho desde cero.'
+    }
+} as const;
