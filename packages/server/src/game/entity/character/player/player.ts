69cf47352fcd58f0d6ce634670b22b281d6d0a1e
Flavius Poenaru <flavius256@icloud.com>
2023-08-17 13:08:04 -0600

    fix(server): save new characters

--- packages/server/src/game/entity/character/player/player.ts
+++ packages/server/src/game/entity/character/player/player.ts
@@ -461,7 +461,11 @@ export default class Player extends Character {
      */
 
     public welcome(): void {
-        if (this.isNew()) return this.notify(`misc:WELCOME;name=${config.name}`);
+        if (this.isNew()) {
+            this.save();
+
+            return this.notify(`misc:WELCOME;name=${config.name}`);
+        }
 
         this.notify(`misc:WELCOME_BACK;name=${config.name}`);
 
