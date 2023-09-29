be308c37a83e73830c63c6e96a8f15fed1bd7fcd
Veradictus <flavius256@icloud.com>
2023-09-28 23:07:26 -0600

    feat: add tagalog

---
+++ packages/common/i18n/tl/crafting.ts
@@ -0,0 +1,8 @@
+export default {
+    INVALID_DATA: 'Hindi wasto ang paggawa, kailangan mo magsumite ng ulat ng bug.',
+    INVALID_ITEM: 'Hindi wasto ang item na iyong napili.',
+    INVALID_ITEMS: 'Mali ang item mo para gawin ito.',
+    SELECT_ITEM: 'Pumili ka ng item para gawin ito.',
+    FAILED_CRAFT: 'Nabigo kang gumawa. {{failedText}}',
+    FAILED_CRAFT_ONE: ' You have failed to craft the item.'
+} as const;
---
+++ packages/common/i18n/tl/enchant.ts
@@ -0,0 +1,7 @@
+export default {
+    NO_ITEM_SELECTED: 'Kailangan mo pumili ng item para makapag enchant.',
+    CANNOT_ENCHANT: 'Ang napili mong item ay di pwedeng ma enchant.',
+    NO_SHARD: 'Kailangan mo ng shard upang ma enchant and item na ito',
+    FAILED_ENCHANT: 'Triny mong mag enchant pero walang nangyare.',
+    SUCCESSFUL_ENCHANT: 'Matagumpay mong na enchant ang iyong item.'
+} as const;
